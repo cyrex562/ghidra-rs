5e4667dd542ed6062f586997aa2b27c574048495
Josh Madden <cyrex562@gmail.com>
2026-06-21 14:14:36 -0400

    port: KeyPath [tests:SKIP] (orig_src/Ghidra/Debug/Framework-TraceModeling/src/main/java/ghidra/trace/model/target/path/KeyPath.java)

--- ghidra-rs/src/trace/model/target/mod.rs
+++ ghidra-rs/src/trace/model/target/mod.rs
@@ -1,2 +1,3 @@
 pub mod duplicate_key_exception;
 pub mod info;
+pub mod path;
---
+++ ghidra-rs/src/trace/model/target/path/mod.rs
@@ -0,0 +1,4 @@
+pub mod key_path;
+pub use key_path::{
+    is_wildcard, KeyComparator, KeyPath, PathComparator, PathFilter, PathParser, ROOT,
+};
