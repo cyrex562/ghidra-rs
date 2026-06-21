102d12fe8838a420897f2536fe9d212b8c7b50bb
Josh Madden <cyrex562@gmail.com>
2026-06-21 14:46:15 -0400

    port: AnyChangeTreeModelListener [tests:SKIP] (orig_src/Ghidra/Debug/ProposedUtils/src/main/java/docking/widgets/tree/AnyChangeTreeModelListener.java)

---
+++ ghidra-rs/src/docking/widgets/tree/mod.rs
@@ -0,0 +1,7 @@
+pub mod any_change_tree_model_listener;
+pub mod tree_model_event;
+pub mod tree_model_listener;
+
+pub use any_change_tree_model_listener::AnyChangeTreeModelListener;
+pub use tree_model_event::TreeModelEvent;
+pub use tree_model_listener::TreeModelListener;
