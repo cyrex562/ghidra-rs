6dc5f6d3897d1998a7c3462cad9ac6687af9b093
Josh Madden <cyrex562@gmail.com>
2026-06-22 23:11:46 -0400

    port: StringDimension [tests:SKIP] (orig_src/Ghidra/Debug/ProposedUtils/src/main/java/ghidra/util/database/spatial/hyper/StringDimension.java)

--- ghidra-rs/src/util/database/spatial/hyper/mod.rs
+++ ghidra-rs/src/util/database/spatial/hyper/mod.rs
@@ -1,5 +1,7 @@
 pub mod hyper_direction;
 pub mod hyper_point;
+pub mod string_dimension;
 
 pub use hyper_direction::HyperDirection;
 pub use hyper_point::HyperPoint;
+pub use string_dimension::StringDimension;
