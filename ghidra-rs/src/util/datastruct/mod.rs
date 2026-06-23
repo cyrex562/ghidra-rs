c63cd321ad1b61230e7588428c3690790d789555
Josh Madden <cyrex562@gmail.com>
2026-06-22 23:35:44 -0400

    port: ObservableCollection [tests:SKIP] (orig_src/Ghidra/Debug/ProposedUtils/src/main/java/ghidra/util/datastruct/ObservableCollection.java)

--- ghidra-rs/src/util/datastruct/mod.rs
+++ ghidra-rs/src/util/datastruct/mod.rs
@@ -1,7 +1,9 @@
 pub mod collection_change_listener;
 pub mod duo;
+pub mod observable_collection;
 pub mod range;
 
 pub use collection_change_listener::CollectionChangeListener;
 pub use duo::{Duo, Side};
+pub use observable_collection::{ChangeAggregator, ObservableCollection};
 pub use range::Range;
