fcdab82ca65adcc8f5aac7c871a9722edc387adb
Josh Madden <cyrex562@gmail.com>
2026-06-22 22:54:55 -0400

    port: SpatialMap [tests:SKIP] (orig_src/Ghidra/Debug/ProposedUtils/src/main/java/ghidra/util/database/spatial/SpatialMap.java)

--- ghidra-rs/src/util/database/spatial/mod.rs
+++ ghidra-rs/src/util/database/spatial/mod.rs
@@ -1,7 +1,9 @@
 pub mod bounded_shape;
 pub mod bounding_shape;
 pub mod query;
+pub mod spatial_map;
 
 pub use bounded_shape::BoundedShape;
 pub use bounding_shape::{union_iterable, BoundingShape};
 pub use query::{Query, QueryInclusion};
+pub use spatial_map::{empty_map, EmptySpatialMap, SpatialMap};
