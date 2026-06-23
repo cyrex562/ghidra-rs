fffa9959dc6589df83144d7f2fcc6f0cb1bc0f3d
Josh Madden <cyrex562@gmail.com>
2026-06-23 00:06:01 -0400

    port: ProxyUtilities [tests:SKIP] (orig_src/Ghidra/Debug/ProposedUtils/src/main/java/utilities/util/ProxyUtilities.java)

--- ghidra-rs/src/util/util/mod.rs
+++ ghidra-rs/src/util/util/mod.rs
@@ -1,5 +1,7 @@
 pub mod id_hashed;
 pub mod id_keyed;
+pub mod proxy_utilities;
 
 pub use id_hashed::IdHashed;
 pub use id_keyed::IdKeyed;
+pub use proxy_utilities::{are_same_method, MethodSignature};
