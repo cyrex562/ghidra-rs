20a8e69f5f7f13f2b564a2e80f732b83ba0e1027
Josh Madden <cyrex562@gmail.com>
2026-06-23 22:47:07 -0400

    port: StructDisplayModel [tests:SKIP] (orig_src/Ghidra/Features/Base/src/main/java/ghidra/app/merge/structures/StructDisplayModel.java)

--- ghidra-rs/src/app/merge/structures/mod.rs
+++ ghidra-rs/src/app/merge/structures/mod.rs
@@ -2,6 +2,7 @@ pub mod comparison_item;
 pub mod comparison_item_layout;
 pub mod coordinated_structure_line;
 pub mod display_coordinator;
+pub mod struct_display_model;
 
 pub use comparison_item::{ComparisonItem, ItemApplyState, MAX_COLS};
 pub use comparison_item_layout::{ColumnWidths, ComparisonItemLayout, HGAP};
@@ -9,3 +10,4 @@ pub use coordinated_structure_line::{
     CompareId, CoordinatedStructureLine, CoordinatedStructureModel,
 };
 pub use display_coordinator::{CoordinatedStructureDisplay, DisplayCoordinator};
+pub use struct_display_model::{StructDisplayDataProvider, StructDisplayModel};
