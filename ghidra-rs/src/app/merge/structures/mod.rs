1999bd1778ad7be622641754161f7f938d45ba92
Josh Madden <cyrex562@gmail.com>
2026-06-23 22:36:01 -0400

    port: CoordinatedStructureLine [tests:SKIP] (orig_src/Ghidra/Features/Base/src/main/java/ghidra/app/merge/structures/CoordinatedStructureLine.java)

--- ghidra-rs/src/app/merge/structures/mod.rs
+++ ghidra-rs/src/app/merge/structures/mod.rs
@@ -1,5 +1,9 @@
 pub mod comparison_item;
 pub mod comparison_item_layout;
+pub mod coordinated_structure_line;
 
 pub use comparison_item::{ComparisonItem, ItemApplyState, MAX_COLS};
 pub use comparison_item_layout::{ColumnWidths, ComparisonItemLayout, HGAP};
+pub use coordinated_structure_line::{
+    CompareId, CoordinatedStructureLine, CoordinatedStructureModel,
+};
