a4c82a63eb2f8c6cad1aff24a672cc7ac8de76fc
Josh Madden <cyrex562@gmail.com>
2026-06-23 23:17:03 -0400

    port: MD5DigestChecksumAlgorithm [tests:SKIP] (orig_src/Ghidra/Features/Base/src/main/java/ghidra/app/plugin/core/checksums/MD5DigestChecksumAlgorithm.java)

--- ghidra-rs/src/app/plugin/core/checksums/mod.rs
+++ ghidra-rs/src/app/plugin/core/checksums/mod.rs
@@ -2,3 +2,4 @@ pub mod checksum8_checksum_algorithm;
 pub mod checksum16_checksum_algorithm;
 pub mod checksum32_checksum_algorithm;
 pub mod md2_digest_checksum_algorithm;
+pub mod md5_digest_checksum_algorithm;
