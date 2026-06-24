5337d97354d0258199c66baf8f5debedfc8e5e34
Josh Madden <cyrex562@gmail.com>
2026-06-23 22:04:27 -0400

    port: Handler [tests:SKIP] (orig_src/Ghidra/Features/BSim/src/main/java/ghidra/features/bsim/query/elastic/Handler.java)

--- ghidra-rs/src/feature/bsim/query/elastic/mod.rs
+++ ghidra-rs/src/feature/bsim/query/elastic/mod.rs
@@ -1,6 +1,7 @@
 pub mod base64_lite;
 pub mod elastic_exception;
 pub mod elastic_utilities;
+pub mod handler;
 
 pub use base64_lite::{
     decode_long_base64, encode_long_base64, encode_long_base64_padded_to_buf,
