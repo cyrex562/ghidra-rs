da42c454322f945c7e51315073f1b93e26415d4a
Josh Madden <cyrex562@gmail.com>
2026-06-25 00:18:27 -0400

    port: DwarfEHDataDecodeFormat [tests:SKIP] (orig_src/Ghidra/Features/Base/src/main/java/ghidra/app/plugin/exceptionhandlers/gcc/DwarfEHDataDecodeFormat.java)

--- ghidra-rs/src/app/plugin/exceptionhandlers/gcc/mod.rs
+++ ghidra-rs/src/app/plugin/exceptionhandlers/gcc/mod.rs
@@ -1,3 +1,5 @@
 pub mod dwarf_eh_data_application_mode;
+pub mod dwarf_eh_data_decode_format;
 
 pub use dwarf_eh_data_application_mode::DwarfEhDataApplicationMode;
+pub use dwarf_eh_data_decode_format::DwarfEhDataDecodeFormat;
