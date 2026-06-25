5131eb3787c1dab6d529f2f2fcd9677139387fc6
Josh Madden <cyrex562@gmail.com>
2026-06-24 22:31:08 -0400

    port: EditorModelListener [tests:SKIP] (orig_src/Ghidra/Features/Base/src/main/java/ghidra/app/plugin/core/compositeeditor/EditorModelListener.java)

--- ghidra-rs/src/app/plugin/core/compositeeditor/mod.rs
+++ ghidra-rs/src/app/plugin/core/compositeeditor/mod.rs
@@ -3,9 +3,11 @@ pub mod composite_change_listener;
 pub mod composite_editor_lock_listener;
 pub mod composite_model_status_listener;
 pub mod composite_viewer_model_listener;
+pub mod editor_model_listener;
 
 pub use component_cell_editor_listener::ComponentCellEditorListener;
 pub use composite_change_listener::CompositeChangeListener;
 pub use composite_editor_lock_listener::CompositeEditorLockListener;
 pub use composite_model_status_listener::CompositeModelStatusListener;
 pub use composite_viewer_model_listener::CompositeViewerModelListener;
+pub use editor_model_listener::EditorModelListener;
