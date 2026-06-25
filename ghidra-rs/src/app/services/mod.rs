d0789f2fb84d5d13e2942eba9c96fc1086b0d828
Josh Madden <cyrex562@gmail.com>
2026-06-25 00:38:59 -0400

    port: BlockModelServiceListener [tests:SKIP] (orig_src/Ghidra/Features/Base/src/main/java/ghidra/app/services/BlockModelServiceListener.java)

--- ghidra-rs/src/app/services/mod.rs
+++ ghidra-rs/src/app/services/mod.rs
@@ -1,5 +1,7 @@
 pub mod analysis_priority;
 pub mod analyzer_type;
+pub mod block_model_service_listener;
 
 pub use analysis_priority::AnalysisPriority;
 pub use analyzer_type::AnalyzerType;
+pub use block_model_service_listener::BlockModelServiceListener;
