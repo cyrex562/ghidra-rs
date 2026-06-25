552856cadcbd6aee873ec56e87c392f6da3748ec
Josh Madden <cyrex562@gmail.com>
2026-06-25 00:41:00 -0400

    port: BookmarkService [tests:SKIP] (orig_src/Ghidra/Features/Base/src/main/java/ghidra/app/services/BookmarkService.java)

--- ghidra-rs/src/app/services/mod.rs
+++ ghidra-rs/src/app/services/mod.rs
@@ -1,7 +1,9 @@
 pub mod analysis_priority;
 pub mod analyzer_type;
 pub mod block_model_service_listener;
+pub mod bookmark_service;
 
 pub use analysis_priority::AnalysisPriority;
 pub use analyzer_type::AnalyzerType;
 pub use block_model_service_listener::BlockModelServiceListener;
+pub use bookmark_service::BookmarkService;
