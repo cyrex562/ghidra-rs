2d5bfebccc1bb7ff429c30896aef1e13811a5aac
Josh Madden <cyrex562@gmail.com>
2026-06-24 22:14:37 -0400

    port: CompositeEditorLockListener [tests:SKIP] (orig_src/Ghidra/Features/Base/src/main/java/ghidra/app/plugin/core/compositeeditor/CompositeEditorLockListener.java)

--- ghidra-rs/src/app/plugin/core/compositeeditor/mod.rs
+++ ghidra-rs/src/app/plugin/core/compositeeditor/mod.rs
@@ -1,5 +1,7 @@
 pub mod component_cell_editor_listener;
 pub mod composite_change_listener;
+pub mod composite_editor_lock_listener;
 
 pub use component_cell_editor_listener::ComponentCellEditorListener;
 pub use composite_change_listener::CompositeChangeListener;
+pub use composite_editor_lock_listener::CompositeEditorLockListener;
