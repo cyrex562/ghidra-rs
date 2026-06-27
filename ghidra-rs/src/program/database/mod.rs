35846b81ca15a98a3abce0322481f7bb6f7072bb
Josh Madden <cyrex562@gmail.com>
2026-06-27 06:11:43 -0400

    port: ProgramModifierListener [tests:SKIP] (orig_src/Ghidra/Features/Base/src/test/java/ghidra/program/database/ProgramModifierListener.java)

--- ghidra-rs/src/program/database/mod.rs
+++ ghidra-rs/src/program/database/mod.rs
@@ -3,9 +3,11 @@ pub mod map;
 pub mod mem;
 pub mod merge_program_generator;
 pub mod program_db;
+pub mod program_modifier_listener;
 pub mod symbol;
 pub mod util;
 
 pub use manager_db::ManagerDB;
 pub use merge_program_generator::MergeProgramGenerator;
 pub use program_db::ProgramDB;
+pub use program_modifier_listener::ProgramModifierListener;
