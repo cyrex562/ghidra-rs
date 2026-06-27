5b3ce3c2f514cbc7e13c0d91cb0f37c81b9c5841
Josh Madden <cyrex562@gmail.com>
2026-06-26 23:07:52 -0400

    port: DataTypeMapperContext [tests:SKIP] (orig_src/Ghidra/Features/Base/src/main/java/ghidra/app/util/bin/format/golang/structmapping/DataTypeMapperContext.java)

--- ghidra-rs/src/format/golang/structmapping/mod.rs
+++ ghidra-rs/src/format/golang/structmapping/mod.rs
@@ -1,5 +1,7 @@
 pub mod after_structure_read;
 pub mod context_field;
+pub mod data_type_mapper_context;
 
 pub use after_structure_read::AfterStructureRead;
 pub use context_field::ContextField;
+pub use data_type_mapper_context::DataTypeMapperContext;
