095e9a219949fdb45059c03453d1437df6e2c0b1
Josh Madden <cyrex562@gmail.com>
2026-06-26 23:16:34 -0400

    port: FieldMapping [tests:SKIP] (orig_src/Ghidra/Features/Base/src/main/java/ghidra/app/util/bin/format/golang/structmapping/FieldMapping.java)

--- ghidra-rs/src/format/golang/structmapping/mod.rs
+++ ghidra-rs/src/format/golang/structmapping/mod.rs
@@ -2,8 +2,10 @@ pub mod after_structure_read;
 pub mod context_field;
 pub mod data_type_mapper_context;
 pub mod eol_comment;
+pub mod field_mapping;
 
 pub use after_structure_read::AfterStructureRead;
 pub use context_field::ContextField;
 pub use data_type_mapper_context::DataTypeMapperContext;
 pub use eol_comment::{EolComment, EolCommentProvider};
+pub use field_mapping::{FieldMapping, Signedness};
