b623b29db690c231f8ebf99a8ea63d5c5e0bd495
Josh Madden <cyrex562@gmail.com>
2026-06-29 02:48:39 -0400

    port: GhidraFileChooserPanelListener [tests:SKIP] (orig_src/Ghidra/Framework/Docking/src/main/java/docking/widgets/filechooser/GhidraFileChooserPanelListener.java)

--- ghidra-rs/src/docking/widgets/filechooser/mod.rs
+++ ghidra-rs/src/docking/widgets/filechooser/mod.rs
@@ -1,7 +1,9 @@
 pub mod ghidra_file;
 pub mod ghidra_file_chooser_directory_model_if;
 pub mod ghidra_file_chooser_mode;
+pub mod ghidra_file_chooser_panel_listener;
 
 pub use ghidra_file::GhidraFile;
 pub use ghidra_file_chooser_directory_model_if::GhidraFileChooserDirectoryModelIf;
 pub use ghidra_file_chooser_mode::GhidraFileChooserMode;
+pub use ghidra_file_chooser_panel_listener::GhidraFileChooserPanelListener;
