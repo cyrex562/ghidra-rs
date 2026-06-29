b72a544f6144021cc5a177e44def2b8ab0d7d1e2
Josh Madden <cyrex562@gmail.com>
2026-06-29 03:07:31 -0400

    port: SelectionStorage [tests:SKIP] (orig_src/Ghidra/Framework/Docking/src/main/java/docking/widgets/table/SelectionStorage.java)

--- ghidra-rs/src/docking/widgets/table/mod.rs
+++ ghidra-rs/src/docking/widgets/table/mod.rs
@@ -3,9 +3,11 @@ pub mod column_annotation;
 pub mod display_string_provider;
 pub mod focusable_editor;
 pub mod object_selected_listener;
+pub mod selection_storage;
 
 pub use add_remove_list_item::{AddRemoveListItem, AddRemoveListItemType};
 pub use column_annotation::ColumnAnnotation;
 pub use display_string_provider::DisplayStringProvider;
 pub use focusable_editor::FocusableEditor;
 pub use object_selected_listener::ObjectSelectedListener;
+pub use selection_storage::SelectionStorage;
