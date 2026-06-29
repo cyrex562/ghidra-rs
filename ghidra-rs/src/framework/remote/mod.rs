2babf584b1287c3b6288e6995d4eba2403a99bca
Josh Madden <cyrex562@gmail.com>
2026-06-29 04:23:27 -0400

    port: SignatureCallback [tests:SKIP] (orig_src/Ghidra/Framework/FileSystem/src/main/java/ghidra/framework/remote/SignatureCallback.java)

--- ghidra-rs/src/framework/remote/mod.rs
+++ ghidra-rs/src/framework/remote/mod.rs
@@ -2,8 +2,10 @@ pub mod anonymous_callback;
 pub mod ghidra_principal;
 pub mod repository_change_event;
 pub mod rmi_server_port_factory;
+pub mod signature_callback;
 
 pub use anonymous_callback::AnonymousCallback;
 pub use ghidra_principal::GhidraPrincipal;
 pub use repository_change_event::{EventType, RepositoryChangeEvent};
 pub use rmi_server_port_factory::RmiServerPortFactory;
+pub use signature_callback::SignatureCallback;
