8cd3c32a9c796d56a4136b76e8f40e65c681b1aa
Josh Madden <cyrex562@gmail.com>
2026-06-29 02:27:44 -0400

    port: DataToStringConverter [tests:SKIP] (orig_src/Ghidra/Framework/Docking/src/main/java/docking/widgets/DataToStringConverter.java)

--- ghidra-rs/src/docking/widgets/mod.rs
+++ ghidra-rs/src/docking/widgets/mod.rs
@@ -1,4 +1,6 @@
 pub mod cursor_position;
+pub mod data_to_string_converter;
 pub mod tree;
 
 pub use cursor_position::CursorPosition;
+pub use data_to_string_converter::{DataToStringConverter, StringDataToStringConverter};
