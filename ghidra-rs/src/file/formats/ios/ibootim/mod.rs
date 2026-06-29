ccf10a9c4ee02a6ca4e1175ef0424c494de3629a
Josh Madden <cyrex562@gmail.com>
2026-06-28 23:03:32 -0400

    port: iBootImInfoHeader [tests:SKIP] (orig_src/Ghidra/Features/FileFormats/src/main/java/ghidra/file/formats/ios/ibootim/iBootImInfoHeader.java)

--- ghidra-rs/src/file/formats/ios/ibootim/mod.rs
+++ ghidra-rs/src/file/formats/ios/ibootim/mod.rs
@@ -1,4 +1,5 @@
 pub mod i_boot_im_constants;
+pub mod i_boot_im_info_header;
 
 pub use i_boot_im_constants::{
     COMPRESSION_LZSS_BE, COMPRESSION_LZSS_LE, FORMAT_ARGB, FORMAT_GREY, HEADER_LENGTH,
