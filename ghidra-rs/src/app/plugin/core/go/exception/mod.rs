85bf1990d6c0d6789983281d81ad88d4f33aeb41
Josh Madden <cyrex562@gmail.com>
2026-06-29 00:00:37 -0400

    port: UnableToGetLockException [tests:SKIP] (orig_src/Ghidra/Features/GhidraGo/src/main/java/ghidra/app/plugin/core/go/exception/UnableToGetLockException.java)

--- ghidra-rs/src/app/plugin/core/go/exception/mod.rs
+++ ghidra-rs/src/app/plugin/core/go/exception/mod.rs
@@ -6,3 +6,6 @@ pub use started_ghidra_process_exited_exception::StartedGhidraProcessExitedExcep
 
 pub mod stop_waiting_exception;
 pub use stop_waiting_exception::StopWaitingException;
+
+pub mod unable_to_get_lock_exception;
+pub use unable_to_get_lock_exception::UnableToGetLockException;
