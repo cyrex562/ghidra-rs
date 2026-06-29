1d5c436a036122c3f7cd2d8a0e5fed7c130df866
Josh Madden <cyrex562@gmail.com>
2026-06-29 04:31:41 -0400

    port: RepositoryLogger [tests:SKIP] (orig_src/Ghidra/Framework/FileSystem/src/main/java/ghidra/framework/store/local/RepositoryLogger.java)

--- ghidra-rs/src/framework/store/local/mod.rs
+++ ghidra-rs/src/framework/store/local/mod.rs
@@ -1,5 +1,7 @@
 pub mod data_directory_exception;
 pub mod file_change_listener;
+pub mod repository_logger;
 
 pub use data_directory_exception::DataDirectoryException;
 pub use file_change_listener::FileChangeListener;
+pub use repository_logger::RepositoryLogger;
