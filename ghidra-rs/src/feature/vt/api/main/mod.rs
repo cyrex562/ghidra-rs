08f157e1b29e9aa1f8ffd48f88be9a51368d2447
Josh Madden <cyrex562@gmail.com>
2026-06-29 01:40:04 -0400

    port: VTMarkupItemStatus [tests:SKIP] (orig_src/Ghidra/Features/VersionTracking/src/main/java/ghidra/feature/vt/api/main/VTMarkupItemStatus.java)

--- ghidra-rs/src/feature/vt/api/main/mod.rs
+++ ghidra-rs/src/feature/vt/api/main/mod.rs
@@ -2,3 +2,4 @@ pub mod vt_association_markup_status;
 pub mod vt_association_status;
 pub mod vt_association_type;
 pub mod vt_markup_item_destination_address_edit_status;
+pub mod vt_markup_item_status;
