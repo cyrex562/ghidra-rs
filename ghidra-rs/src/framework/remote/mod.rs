bbe3d9e4419f66c6e2b74e7e42fffb10ae74bbcf
Josh Madden <cyrex562@gmail.com>
2026-06-29 04:25:54 -0400

    port: User [tests:SKIP] (orig_src/Ghidra/Framework/FileSystem/src/main/java/ghidra/framework/remote/User.java)

--- ghidra-rs/src/framework/remote/mod.rs
+++ ghidra-rs/src/framework/remote/mod.rs
@@ -3,9 +3,11 @@ pub mod ghidra_principal;
 pub mod repository_change_event;
 pub mod rmi_server_port_factory;
 pub mod signature_callback;
+pub mod user;
 
 pub use anonymous_callback::AnonymousCallback;
 pub use ghidra_principal::GhidraPrincipal;
 pub use repository_change_event::{EventType, RepositoryChangeEvent};
 pub use rmi_server_port_factory::RmiServerPortFactory;
 pub use signature_callback::SignatureCallback;
+pub use user::{Permission, User, ANONYMOUS_USERNAME};
