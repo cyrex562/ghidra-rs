0a9f6dfdf8e643134f8d5e3f346273a9f3443783
Josh Madden <cyrex562@gmail.com>
2026-06-29 02:51:54 -0400

    port: TermSplitter [tests:SKIP] (orig_src/Ghidra/Framework/Docking/src/main/java/docking/widgets/filter/TermSplitter.java)

--- ghidra-rs/src/docking/widgets/filter/mod.rs
+++ ghidra-rs/src/docking/widgets/filter/mod.rs
@@ -1,5 +1,7 @@
 pub mod filter_listener;
 pub mod multiterm_evaluation_mode;
+pub mod term_splitter;
 
 pub use filter_listener::FilterListener;
 pub use multiterm_evaluation_mode::MultitermEvaluationMode;
+pub use term_splitter::TermSplitter;
