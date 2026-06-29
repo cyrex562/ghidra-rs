0623d42f62ed62559a5502f11caadb0e3956b9e5
Josh Madden <cyrex562@gmail.com>
2026-06-29 01:43:21 -0400

    port: VTProgramCorrelatorAddressRestrictionPreference [tests:SKIP] (orig_src/Ghidra/Features/VersionTracking/src/main/java/ghidra/feature/vt/api/main/VTProgramCorrelatorAddressRestrictionPreference.java)

--- ghidra-rs/src/feature/vt/api/main/mod.rs
+++ ghidra-rs/src/feature/vt/api/main/mod.rs
@@ -4,3 +4,4 @@ pub mod vt_association_type;
 pub mod vt_markup_item_destination_address_edit_status;
 pub mod vt_markup_item_status;
 pub mod vt_match_tag;
+pub mod vt_program_correlator_address_restriction_preference;
