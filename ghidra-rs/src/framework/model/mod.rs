a54897d28a376555a4ca43836ebc178ea87be006
Josh Madden <cyrex562@gmail.com>
2026-06-30 12:27:08 -0400

    port: DomainObjectLockedException [tests:SKIP] (orig_src/Ghidra/Framework/Project/src/main/java/ghidra/framework/model/DomainObjectLockedException.java)

--- ghidra-rs/src/framework/model/mod.rs
+++ ghidra-rs/src/framework/model/mod.rs
@@ -2,10 +2,12 @@ pub mod aborted_transaction_listener;
 pub mod change_set;
 pub mod domain_object_event_id_generator;
 pub mod domain_object_exception;
+pub mod domain_object_locked_exception;
 pub mod server_info;
 
 pub use aborted_transaction_listener::AbortedTransactionListener;
 pub use change_set::ChangeSet;
 pub use domain_object_event_id_generator::DomainObjectEventIdGenerator;
 pub use domain_object_exception::DomainObjectException;
+pub use domain_object_locked_exception::DomainObjectLockedException;
 pub use server_info::ServerInfo;
