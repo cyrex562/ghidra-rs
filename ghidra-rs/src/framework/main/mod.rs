e1e204bfe7afc876f447f18b831a0886eaeea174
Josh Madden <cyrex562@gmail.com>
2026-06-30 12:14:47 -0400

    port: Cuttable [tests:SKIP] (orig_src/Ghidra/Framework/Project/src/main/java/ghidra/framework/main/datatree/Cuttable.java)

--- ghidra-rs/src/framework/main/mod.rs
+++ ghidra-rs/src/framework/main/mod.rs
@@ -1,9 +1,11 @@
 pub mod application_level_plugin;
 pub mod console_listener;
 pub mod data_tree_dialog_type;
+pub mod datatree;
 pub mod programatic_use_only;
 
 pub use application_level_plugin::ApplicationLevelPlugin;
 pub use console_listener::ConsoleListener;
 pub use data_tree_dialog_type::DataTreeDialogType;
+pub use datatree::Cuttable;
 pub use programatic_use_only::ProgramaticUseOnly;
