40f444f707e5670ff8474dfd0a94cc933c8c925f
Josh Madden <cyrex562@gmail.com>
2026-06-30 12:22:58 -0400

    port: ChangeSet [tests:SKIP] (orig_src/Ghidra/Framework/Project/src/main/java/ghidra/framework/model/ChangeSet.java)

--- ghidra-rs/src/framework/model/mod.rs
+++ ghidra-rs/src/framework/model/mod.rs
@@ -1,5 +1,7 @@
 pub mod aborted_transaction_listener;
+pub mod change_set;
 pub mod server_info;
 
 pub use aborted_transaction_listener::AbortedTransactionListener;
+pub use change_set::ChangeSet;
 pub use server_info::ServerInfo;
