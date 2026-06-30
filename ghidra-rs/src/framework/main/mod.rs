3b25502bdeebb907cab10808f328606d04daa569
Josh Madden <cyrex562@gmail.com>
2026-06-30 12:12:34 -0400

    port: ProgramaticUseOnly [tests:SKIP] (orig_src/Ghidra/Framework/Project/src/main/java/ghidra/framework/main/ProgramaticUseOnly.java)

--- ghidra-rs/src/framework/main/mod.rs
+++ ghidra-rs/src/framework/main/mod.rs
@@ -1,7 +1,9 @@
 pub mod application_level_plugin;
 pub mod console_listener;
 pub mod data_tree_dialog_type;
+pub mod programatic_use_only;
 
 pub use application_level_plugin::ApplicationLevelPlugin;
 pub use console_listener::ConsoleListener;
 pub use data_tree_dialog_type::DataTreeDialogType;
+pub use programatic_use_only::ProgramaticUseOnly;
