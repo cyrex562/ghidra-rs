f58f2018e52f933c26967c6e7288d43205ce24e2
Josh Madden <cyrex562@gmail.com>
2026-06-30 13:16:41 -0400

    port: SolverException [tests:SKIP] (orig_src/Ghidra/Framework/SoftwareModeling/src/main/java/ghidra/app/plugin/assembler/sleigh/expr/SolverException.java)

--- ghidra-rs/src/app/plugin/assembler/mod.rs
+++ ghidra-rs/src/app/plugin/assembler/mod.rs
@@ -1,5 +1,6 @@
 pub mod assembly_error;
 pub mod assembly_exception;
+pub mod sleigh;
 
 pub use assembly_error::AssemblyError;
 pub use assembly_exception::AssemblyException;
---
+++ ghidra-rs/src/app/plugin/assembler/sleigh/expr/mod.rs
@@ -0,0 +1,3 @@
+pub mod solver_exception;
+
+pub use solver_exception::SolverException;
