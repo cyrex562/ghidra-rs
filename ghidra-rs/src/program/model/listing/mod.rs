781d287618c6d520daa29210e2c68c0c57b3029d
Josh Madden <cyrex562@gmail.com>
2026-06-30 15:08:44 -0400

    port: FunctionTag [tests:SKIP] (orig_src/Ghidra/Framework/SoftwareModeling/src/main/java/ghidra/program/model/listing/FunctionTag.java)

--- ghidra-rs/src/program/model/listing/mod.rs
+++ ghidra-rs/src/program/model/listing/mod.rs
@@ -1,7 +1,9 @@
 pub mod auto_parameter_type;
 pub mod domain_object_change_set;
+pub mod function_tag;
 pub mod program;
 
 pub use auto_parameter_type::AutoParameterType;
 pub use domain_object_change_set::DomainObjectChangeSet;
+pub use function_tag::FunctionTag;
 pub use program::Program;
