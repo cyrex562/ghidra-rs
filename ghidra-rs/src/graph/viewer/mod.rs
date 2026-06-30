501b55b4f933131ceb4cc41cf67cc2ae9b84cf7f
Josh Madden <cyrex562@gmail.com>
2026-06-30 11:43:00 -0400

    port: RelayoutOption [tests:SKIP] (orig_src/Ghidra/Framework/Graph/src/main/java/ghidra/graph/viewer/options/RelayoutOption.java)

--- ghidra-rs/src/graph/viewer/mod.rs
+++ ghidra-rs/src/graph/viewer/mod.rs
@@ -2,8 +2,10 @@ pub mod actions;
 pub mod edge;
 pub mod event;
 pub mod layout;
+pub mod options;
 
 pub use actions::VisualGraphContextMarker;
 pub use edge::PathHighlightListener;
 pub use event::{EventSource, PickListener};
 pub use layout::GridRange;
+pub use options::RelayoutOption;
