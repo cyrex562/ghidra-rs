b8ae39f2d0a38590b0ca0f1af1b000370ace2d29
Josh Madden <cyrex562@gmail.com>
2026-06-30 15:57:43 -0400

    port: LexerMultiplexer [tests:SKIP] (orig_src/Ghidra/Framework/SoftwareModeling/src/main/java/ghidra/sleigh/grammar/LexerMultiplexer.java)

--- ghidra-rs/src/sleigh/grammar/mod.rs
+++ ghidra-rs/src/sleigh/grammar/mod.rs
@@ -1,7 +1,9 @@
 pub mod bailout_exception;
 pub mod conditional_helper;
 pub mod expression_environment;
+pub mod lexer_multiplexer;
 
 pub use bailout_exception::BailoutException;
 pub use conditional_helper::ConditionalHelper;
 pub use expression_environment::ExpressionEnvironment;
+pub use lexer_multiplexer::{LexerMultiplexer, Token, TokenSource, DEFAULT_CHANNEL};
