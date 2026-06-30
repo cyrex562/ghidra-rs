4cdd8090fdb87ffc366ba28fb893eb982307b27c
Josh Madden <cyrex562@gmail.com>
2026-06-30 11:53:44 -0400

    port: LayoutAlgorithmNames [tests:SKIP] (orig_src/Ghidra/Framework/Graph/src/main/java/ghidra/service/graph/LayoutAlgorithmNames.java)

--- ghidra-rs/src/service/graph/mod.rs
+++ ghidra-rs/src/service/graph/mod.rs
@@ -1,7 +1,9 @@
 pub mod attributed;
 pub mod graph_label_position;
 pub mod graph_type;
+pub mod layout_algorithm_names;
 
 pub use attributed::Attributed;
 pub use graph_label_position::GraphLabelPosition;
 pub use graph_type::GraphType;
+pub use layout_algorithm_names::get_layout_algorithm_names;
