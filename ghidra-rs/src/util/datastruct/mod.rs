1331b8c3c877c917dfec40c9696756584d905ae8
Josh Madden <cyrex562@gmail.com>
2026-06-30 09:56:47 -0400

    port: IndexRange [tests:SKIP] (orig_src/Ghidra/Framework/Generic/src/main/java/ghidra/util/datastruct/IndexRange.java)

--- ghidra-rs/src/util/datastruct/mod.rs
+++ ghidra-rs/src/util/datastruct/mod.rs
@@ -5,6 +5,7 @@ pub mod case_insensitive_duplicate_string_comparator;
 pub mod collection_change_listener;
 pub mod counter;
 pub mod duo;
+pub mod index_range;
 pub mod observable_collection;
 pub mod privately_queued_listener;
 pub mod range;
@@ -16,6 +17,7 @@ pub use case_insensitive_duplicate_string_comparator::CaseInsensitiveDuplicateSt
 pub use collection_change_listener::CollectionChangeListener;
 pub use counter::Counter;
 pub use duo::{Duo, Side};
+pub use index_range::IndexRange;
 pub use observable_collection::{ChangeAggregator, ObservableCollection};
 pub use privately_queued_listener::{
     DefaultListenerErrorHandler, ListenerErrorHandler, PrivatelyQueuedListener,
