09775e52b845ecab2523e9a6ffd8d49d429e7a47
Josh Madden <cyrex562@gmail.com>
2026-06-30 09:47:04 -0400

    port: AbstractWeakValueMap [tests:SKIP] (orig_src/Ghidra/Framework/Generic/src/main/java/ghidra/util/datastruct/AbstractWeakValueMap.java)

--- ghidra-rs/src/util/datastruct/mod.rs
+++ ghidra-rs/src/util/datastruct/mod.rs
@@ -1,9 +1,11 @@
+pub mod abstract_weak_value_map;
 pub mod collection_change_listener;
 pub mod duo;
 pub mod observable_collection;
 pub mod privately_queued_listener;
 pub mod range;
 
+pub use abstract_weak_value_map::{AbstractWeakValueMap, WeakRefStore};
 pub use collection_change_listener::CollectionChangeListener;
 pub use duo::{Duo, Side};
 pub use observable_collection::{ChangeAggregator, ObservableCollection};
