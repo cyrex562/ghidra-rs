e4fba14aa358caf60eeaeb7f50c66ea61d1dde07
Josh Madden <cyrex562@gmail.com>
2026-06-30 15:01:19 -0400

    port: ProcessorNotFoundException [tests:SKIP] (orig_src/Ghidra/Framework/SoftwareModeling/src/main/java/ghidra/program/model/lang/ProcessorNotFoundException.java)

--- ghidra-rs/src/program/model/lang/mod.rs
+++ ghidra-rs/src/program/model/lang/mod.rs
@@ -3,9 +3,11 @@ pub mod endian;
 pub mod input_list_type;
 pub mod language_id;
 pub mod operand_type;
+pub mod processor_not_found_exception;
 pub mod sleigh;
 
 pub use endian::Endian;
 pub use input_list_type::InputListType;
 pub use language_id::LanguageID;
 pub use operand_type::OperandType;
+pub use processor_not_found_exception::ProcessorNotFoundException;
