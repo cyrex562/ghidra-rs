b8d7423c1808334f2311a69b2c0aa8259642b256
Josh Madden <cyrex562@gmail.com>
2026-06-30 15:47:38 -0400

    port: ProcessorSymbolType [tests:SKIP] (orig_src/Ghidra/Framework/SoftwareModeling/src/main/java/ghidra/program/model/util/ProcessorSymbolType.java)

--- ghidra-rs/src/program/util/mod.rs
+++ ghidra-rs/src/program/util/mod.rs
@@ -1,5 +1,7 @@
 pub mod deleted_exception;
+pub mod processor_symbol_type;
 pub mod program_diff_filter;
 
 pub use deleted_exception::DeletedException;
+pub use processor_symbol_type::ProcessorSymbolType;
 pub use program_diff_filter::ProgramDiffFilter;
