d0ee197579ab266862772be870194f0cd1b3dc46
Josh Madden <cyrex562@gmail.com>
2026-06-30 11:52:16 -0400

    port: GraphType [tests:SKIP] (orig_src/Ghidra/Framework/Graph/src/main/java/ghidra/service/graph/GraphType.java)

--- ghidra-rs/src/service/graph/mod.rs
+++ ghidra-rs/src/service/graph/mod.rs
@@ -1,5 +1,7 @@
 pub mod attributed;
 pub mod graph_label_position;
+pub mod graph_type;
 
 pub use attributed::Attributed;
 pub use graph_label_position::GraphLabelPosition;
+pub use graph_type::GraphType;
