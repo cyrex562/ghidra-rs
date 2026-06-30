2dca0dcd0ba34397b30b9393c13ffb657f511955
Josh Madden <cyrex562@gmail.com>
2026-06-30 11:41:11 -0400

    port: GridRange [tests:SKIP] (orig_src/Ghidra/Framework/Graph/src/main/java/ghidra/graph/viewer/layout/GridRange.java)

--- ghidra-rs/src/graph/viewer/mod.rs
+++ ghidra-rs/src/graph/viewer/mod.rs
@@ -1,7 +1,9 @@
 pub mod actions;
 pub mod edge;
 pub mod event;
+pub mod layout;
 
 pub use actions::VisualGraphContextMarker;
 pub use edge::PathHighlightListener;
 pub use event::{EventSource, PickListener};
+pub use layout::GridRange;
