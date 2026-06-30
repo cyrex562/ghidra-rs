3cbc1d6852cc2c0f4767e3e6dec0535ab80e6b37
Josh Madden <cyrex562@gmail.com>
2026-06-30 09:49:03 -0400

    port: Accumulator [tests:SKIP] (orig_src/Ghidra/Framework/Generic/src/main/java/ghidra/util/datastruct/Accumulator.java)

--- ghidra-rs/src/util/datastruct/mod.rs
+++ ghidra-rs/src/util/datastruct/mod.rs
@@ -1,4 +1,5 @@
 pub mod abstract_weak_value_map;
+pub mod accumulator;
 pub mod collection_change_listener;
 pub mod duo;
 pub mod observable_collection;
@@ -6,6 +7,7 @@ pub mod privately_queued_listener;
 pub mod range;
 
 pub use abstract_weak_value_map::{AbstractWeakValueMap, WeakRefStore};
+pub use accumulator::Accumulator;
 pub use collection_change_listener::CollectionChangeListener;
 pub use duo::{Duo, Side};
 pub use observable_collection::{ChangeAggregator, ObservableCollection};
