0d10a2f2150c374e73b25017a37c1a6c2a73dd55
Josh Madden <cyrex562@gmail.com>
2026-06-30 23:47:00 -0400

    port: TestingPlugin [tests:SKIP] (orig_src/Ghidra/Test/IntegrationTest/src/test.slow/java/ghidra/framework/plugintool/TestingPlugin.java)

--- ghidra-rs/src/framework/plugintool/mod.rs
+++ ghidra-rs/src/framework/plugintool/mod.rs
@@ -1,8 +1,10 @@
 pub mod annotation;
 pub mod busy_tool_exception;
+pub mod testing_plugin;
 pub mod tool_event_name;
 pub mod util;
 
 pub use busy_tool_exception::BusyToolException;
+pub use testing_plugin::TestingPlugin;
 pub use tool_event_name::ToolEventName;
 pub use util::{PluginPackageState, ServiceListener};
