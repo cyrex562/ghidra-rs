2e6e8686f5cb3feb9f5d866fa746a8d3086074a0
Josh Madden <cyrex562@gmail.com>
2026-07-01 04:58:30 -0400

    port: PtyParent [tests:SKIP] (orig_src/Ghidra/Framework/Pty/src/main/java/ghidra/pty/PtyParent.java)

--- ghidra-rs/src/pty/mod.rs
+++ ghidra-rs/src/pty/mod.rs
@@ -1,9 +1,11 @@
 pub mod pty_endpoint;
+pub mod pty_parent;
 pub mod pty_session;
 pub mod shell_utils;
 pub mod stream_pumper;
 pub mod windows;
 
 pub use pty_endpoint::PtyEndpoint;
+pub use pty_parent::PtyParent;
 pub use pty_session::PtySession;
 pub use stream_pumper::StreamPumper;
