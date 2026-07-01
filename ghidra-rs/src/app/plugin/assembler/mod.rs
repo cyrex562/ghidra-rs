d7657ca8ed652aa0f4d8e732f4e0807c7ccbcfd4
Josh Madden <cyrex562@gmail.com>
2026-07-01 05:15:12 -0400

    port: AssemblySelectionError [tests:SKIP] (orig_src/Ghidra/Framework/SoftwareModeling/src/main/java/ghidra/app/plugin/assembler/AssemblySelectionError.java)

--- ghidra-rs/src/app/plugin/assembler/mod.rs
+++ ghidra-rs/src/app/plugin/assembler/mod.rs
@@ -1,6 +1,8 @@
 pub mod assembly_error;
 pub mod assembly_exception;
+pub mod assembly_selection_error;
 pub mod sleigh;
 
 pub use assembly_error::AssemblyError;
 pub use assembly_exception::AssemblyException;
+pub use assembly_selection_error::AssemblySelectionError;
