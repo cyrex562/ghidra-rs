a1108b5dd6aae74636f22bb1bf857c46fa3369cc
Josh Madden <cyrex562@gmail.com>
2026-07-01 02:27:45 -0400

    port: LocalDataFileHandle [tests:SKIP] (orig_src/Ghidra/Framework/FileSystem/src/main/java/ghidra/framework/store/local/LocalDataFileHandle.java)

--- ghidra-rs/src/framework/store/local/mod.rs
+++ ghidra-rs/src/framework/store/local/mod.rs
@@ -1,7 +1,9 @@
 pub mod data_directory_exception;
 pub mod file_change_listener;
+pub mod local_data_file_handle;
 pub mod repository_logger;
 
 pub use data_directory_exception::DataDirectoryException;
 pub use file_change_listener::FileChangeListener;
+pub use local_data_file_handle::LocalDataFileHandle;
 pub use repository_logger::RepositoryLogger;
