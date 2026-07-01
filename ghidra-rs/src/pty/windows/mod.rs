d9c559606e704dadf2f6c6507bfa21549fb2376d
Josh Madden <cyrex562@gmail.com>
2026-07-01 05:08:15 -0400

    port: Pipe [tests:SKIP] (orig_src/Ghidra/Framework/Pty/src/main/java/ghidra/pty/windows/Pipe.java)

--- ghidra-rs/src/pty/windows/mod.rs
+++ ghidra-rs/src/pty/windows/mod.rs
@@ -2,8 +2,10 @@ pub mod handle;
 pub mod handle_input_stream;
 pub mod handle_output_stream;
 pub mod jna;
+pub mod pipe;
 
 pub use handle::Handle;
 pub use handle::RawHandle;
 pub use handle_input_stream::HandleInputStream;
 pub use handle_output_stream::HandleOutputStream;
+pub use pipe::Pipe;
