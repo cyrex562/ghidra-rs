f13256a163870c1d07b131e9a79ecc0ca3cd27b3
Josh Madden <cyrex562@gmail.com>
2026-07-01 01:52:48 -0400

    port: AbstractPatternTextFilter [tests:SKIP] (orig_src/Ghidra/Framework/Docking/src/main/java/docking/widgets/filter/AbstractPatternTextFilter.java)

--- ghidra-rs/src/docking/widgets/filter/mod.rs
+++ ghidra-rs/src/docking/widgets/filter/mod.rs
@@ -1,9 +1,11 @@
+pub mod abstract_pattern_text_filter;
 pub mod filter_listener;
 pub mod multiterm_evaluation_mode;
 pub mod term_splitter;
 pub mod text_filter;
 pub mod text_filter_strategy;
 
+pub use abstract_pattern_text_filter::AbstractPatternTextFilter;
 pub use filter_listener::FilterListener;
 pub use multiterm_evaluation_mode::MultitermEvaluationMode;
 pub use term_splitter::TermSplitter;
