8c696383d9276199301e43f089841f1971b601a8
Josh Madden <cyrex562@gmail.com>
2026-07-01 00:25:46 -0400

    port: DBSynchronizedSpliterator [tests:SKIP] (orig_src/Ghidra/Debug/ProposedUtils/src/main/java/ghidra/util/database/DBSynchronizedSpliterator.java)

--- ghidra-rs/src/util/database/mod.rs
+++ ghidra-rs/src/util/database/mod.rs
@@ -3,6 +3,8 @@ pub mod err;
 pub mod spatial;
 pub mod synchronized_spliterator;
 pub mod db_synchronized_iterator;
+pub mod db_synchronized_spliterator;
 
 pub use synchronized_spliterator::{Spliterator, SynchronizedSpliterator};
 pub use db_synchronized_iterator::{DBSynchronizedIterator, RemovableIterator};
+pub use db_synchronized_spliterator::DBSynchronizedSpliterator;
