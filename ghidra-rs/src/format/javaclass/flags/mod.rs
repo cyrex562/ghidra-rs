80957f9d878c1302036be20c12c9e9a7a43f5973
Josh Madden <cyrex562@gmail.com>
2026-06-30 23:39:39 -0400

    port: FieldInfoAccessFlags [tests:SKIP] (orig_src/Ghidra/Processors/JVM/src/main/java/ghidra/javaclass/flags/FieldInfoAccessFlags.java)

--- ghidra-rs/src/format/javaclass/flags/mod.rs
+++ ghidra-rs/src/format/javaclass/flags/mod.rs
@@ -1,3 +1,5 @@
 pub mod class_file_flags;
+pub mod field_info_access_flags;
 
 pub use class_file_flags::{ACC_ABSTRACT, ACC_ANNOTATION, ACC_ENUM, ACC_FINAL, ACC_INTERFACE, ACC_PUBLIC, ACC_SUPER, ACC_SYNTHETIC};
+pub use field_info_access_flags::{ACC_PRIVATE, ACC_PROTECTED, ACC_STATIC, ACC_TRANSIENT, ACC_VOLATILE};
