db0037c80293451f28e54b6a42138bcab646535b
Josh Madden <cyrex562@gmail.com>
2026-07-01 02:22:40 -0400

    port: FileIDFactory [tests:SKIP] (orig_src/Ghidra/Framework/FileSystem/src/main/java/ghidra/framework/store/FileIDFactory.java)

--- ghidra-rs/src/framework/store/mod.rs
+++ ghidra-rs/src/framework/store/mod.rs
@@ -1,9 +1,11 @@
 pub mod data_file_handle;
 pub mod exclusive_checkout_exception;
+pub mod file_id_factory;
 pub mod local;
 pub mod version;
 
 pub use data_file_handle::DataFileHandle;
 pub use exclusive_checkout_exception::ExclusiveCheckoutException;
+pub use file_id_factory::FileIDFactory;
 pub use local::DataDirectoryException;
 pub use version::ItemVersion;
