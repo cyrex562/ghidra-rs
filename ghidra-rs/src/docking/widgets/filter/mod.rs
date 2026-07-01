cb602bb5c532ec641b2c031ca417d608a29e953a
Josh Madden <cyrex562@gmail.com>
2026-07-01 01:54:28 -0400

    port: InvertedTextFilter [tests:SKIP] (orig_src/Ghidra/Framework/Docking/src/main/java/docking/widgets/filter/InvertedTextFilter.java)

--- ghidra-rs/src/docking/widgets/filter/mod.rs
+++ ghidra-rs/src/docking/widgets/filter/mod.rs
@@ -1,5 +1,6 @@
 pub mod abstract_pattern_text_filter;
 pub mod filter_listener;
+pub mod inverted_text_filter;
 pub mod multiterm_evaluation_mode;
 pub mod term_splitter;
 pub mod text_filter;
@@ -7,6 +8,7 @@ pub mod text_filter_strategy;
 
 pub use abstract_pattern_text_filter::AbstractPatternTextFilter;
 pub use filter_listener::FilterListener;
+pub use inverted_text_filter::InvertedTextFilter;
 pub use multiterm_evaluation_mode::MultitermEvaluationMode;
 pub use term_splitter::TermSplitter;
 pub use text_filter::TextFilter;
