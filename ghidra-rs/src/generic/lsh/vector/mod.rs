1c14f74fc9366733f98fc988955d761c3aedda2b
Josh Madden <cyrex562@gmail.com>
2026-07-02 04:50:22 -0400

    port: HashEntry [tests:SKIP] (orig_src/Ghidra/Framework/Generic/src/main/java/generic/lsh/vector/HashEntry.java)

--- ghidra-rs/src/generic/lsh/vector/mod.rs
+++ ghidra-rs/src/generic/lsh/vector/mod.rs
@@ -1,5 +1,7 @@
+pub mod hash_entry;
 pub mod idf_lookup;
 pub mod weight_factory;
 
+pub use hash_entry::HashEntry;
 pub use idf_lookup::{IdfEntry, IdfLookup};
 pub use weight_factory::WeightFactory;
