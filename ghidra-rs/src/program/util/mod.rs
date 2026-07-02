dbb2f459fba43e52ccf0cdedeb155ec07ad51dff
Josh Madden <cyrex562@gmail.com>
2026-07-02 10:25:44 -0400

    port: CodeUnitInsertionException [tests:SKIP] (orig_src/Ghidra/Framework/SoftwareModeling/src/main/java/ghidra/program/model/util/CodeUnitInsertionException.java)

--- ghidra-rs/src/program/util/mod.rs
+++ ghidra-rs/src/program/util/mod.rs
@@ -1,8 +1,10 @@
+pub mod code_unit_insertion_exception;
 pub mod deleted_exception;
 pub mod processor_symbol_type;
 pub mod program_conflict_exception;
 pub mod program_diff_filter;
 
+pub use code_unit_insertion_exception::CodeUnitInsertionException;
 pub use deleted_exception::DeletedException;
 pub use processor_symbol_type::ProcessorSymbolType;
 pub use program_conflict_exception::ProgramConflictException;
