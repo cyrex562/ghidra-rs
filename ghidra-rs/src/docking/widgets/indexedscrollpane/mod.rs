310d79c0677b556c4277fb224bc89a65d245a3e1
Josh Madden <cyrex562@gmail.com>
2026-07-02 01:01:09 -0400

    port: UniformViewToIndexMapper [tests:SKIP] (orig_src/Ghidra/Framework/Docking/src/main/java/docking/widgets/indexedscrollpane/UniformViewToIndexMapper.java)

--- ghidra-rs/src/docking/widgets/indexedscrollpane/mod.rs
+++ ghidra-rs/src/docking/widgets/indexedscrollpane/mod.rs
@@ -2,10 +2,12 @@ pub mod default_view_to_index_mapper;
 pub mod index_scroll_listener;
 pub mod indexed_scrollable;
 pub mod pre_mapped_view_to_index_mapper;
+pub mod uniform_view_to_index_mapper;
 pub mod view_to_index_mapper;
 
 pub use default_view_to_index_mapper::DefaultViewToIndexMapper;
 pub use index_scroll_listener::IndexScrollListener;
 pub use indexed_scrollable::{IndexedScrollable, IndexScrollListenerAdapter};
 pub use pre_mapped_view_to_index_mapper::PreMappedViewToIndexMapper;
+pub use uniform_view_to_index_mapper::UniformViewToIndexMapper;
 pub use view_to_index_mapper::ViewToIndexMapper;
