1c8469f7cd9878fcd75ee7ba06dea73884c9e20b
Josh Madden <cyrex562@gmail.com>
2026-07-02 04:46:22 -0400

    port: DependentServiceResolverTest [tests:SKIP] (orig_src/Ghidra/Debug/ProposedUtils/src/test/java/generic/depends/DependentServiceResolverTest.java)

--- ghidra-rs/src/generic/depends/mod.rs
+++ ghidra-rs/src/generic/depends/mod.rs
@@ -3,6 +3,9 @@ pub mod dependent_service_constructor;
 pub mod dependent_service_resolver;
 pub mod err;
 
+#[cfg(test)]
+mod dependent_service_resolver_test;
+
 pub use dependent_service::{DependentService, Sentinel};
 pub use dependent_service_constructor::DependentServiceConstructor;
 pub use dependent_service_resolver::{
