353aadfdefb1880c70c4076074dafd897bde2503
Josh Madden <cyrex562@gmail.com>
2026-07-02 10:36:36 -0400

    port: IndexedByteBlockInfo [tests:SKIP] (orig_src/Ghidra/Features/ByteViewer/src/main/java/ghidra/app/plugin/core/format/IndexedByteBlockInfo.java)

--- ghidra-rs/src/app/plugin/core/format/mod.rs
+++ ghidra-rs/src/app/plugin/core/format/mod.rs
@@ -3,9 +3,11 @@ pub mod byte_block_access_exception;
 pub mod byte_block_info;
 pub mod byte_block_range;
 pub mod byte_block_selection;
+pub mod indexed_byte_block_info;
 
 pub use byte_block::ByteBlock;
 pub use byte_block_access_exception::ByteBlockAccessException;
 pub use byte_block_info::ByteBlockInfo;
 pub use byte_block_range::ByteBlockRange;
 pub use byte_block_selection::ByteBlockSelection;
+pub use indexed_byte_block_info::IndexedByteBlockInfo;
