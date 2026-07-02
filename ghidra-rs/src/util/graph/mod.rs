95041f730aff194dbb695575ccf96303caa16626
Josh Madden <cyrex562@gmail.com>
2026-07-02 10:54:04 -0400

    port: AddableLongDoubleHashtable [tests:SKIP] (orig_src/Ghidra/Framework/Generic/src/main/java/ghidra/util/graph/AddableLongDoubleHashtable.java)

--- ghidra-rs/src/util/graph/mod.rs
+++ ghidra-rs/src/util/graph/mod.rs
@@ -1,4 +1,7 @@
+pub mod addable_long_double_hashtable;
 pub mod path;
 
+#[allow(deprecated)]
+pub use addable_long_double_hashtable::AddableLongDoubleHashtable;
 #[allow(deprecated)]
 pub use path::Path;
