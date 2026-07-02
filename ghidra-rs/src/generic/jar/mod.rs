467f6a63360f8d541849167ac840a93d501c1b9a
Josh Madden <cyrex562@gmail.com>
2026-07-01 23:19:23 -0400

    port: GClassLoader [tests:SKIP] (orig_src/Ghidra/Framework/Utility/src/main/java/generic/jar/GClassLoader.java)

--- ghidra-rs/src/generic/jar/mod.rs
+++ ghidra-rs/src/generic/jar/mod.rs
@@ -2,8 +2,10 @@ pub mod resource;
 pub mod resource_file;
 pub mod jar_entry_filter;
 pub mod jar_entry_node;
+pub mod g_class_loader;
 
 pub use resource::{FileResource, Resource};
 pub use resource_file::ResourceFile;
 pub use jar_entry_filter::{JarEntry, JarEntryFilter};
 pub use jar_entry_node::{JarEntryNode, JarFileAccess, NodeRef};
+pub use g_class_loader::GClassLoader;
