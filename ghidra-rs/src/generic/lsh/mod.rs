d7f7428c6f497ad02e49512bfdb3a66f14903b51
Josh Madden <cyrex562@gmail.com>
2026-07-02 02:28:23 -0400

    port: WeightFactory [tests:SKIP] (orig_src/Ghidra/Framework/Generic/src/main/java/generic/lsh/vector/WeightFactory.java)

--- ghidra-rs/src/generic/lsh/mod.rs
+++ ghidra-rs/src/generic/lsh/mod.rs
@@ -1,5 +1,7 @@
 pub mod kand_l;
 pub mod lsh_memory_model;
+pub mod vector;
 
 pub use kand_l::{k_to_l, memory_model_to_l};
 pub use lsh_memory_model::LshMemoryModel;
+pub use vector::WeightFactory;
---
+++ ghidra-rs/src/generic/lsh/vector/mod.rs
@@ -0,0 +1,3 @@
+pub mod weight_factory;
+
+pub use weight_factory::WeightFactory;
