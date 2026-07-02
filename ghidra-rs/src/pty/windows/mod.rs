028dd9d1c744f6c76f0c09f55656d32093638d48
Josh Madden <cyrex562@gmail.com>
2026-07-02 01:53:40 -0400

    port: PseudoConsoleHandle [tests:SKIP] (orig_src/Ghidra/Framework/Pty/src/main/java/ghidra/pty/windows/PseudoConsoleHandle.java)

--- ghidra-rs/src/pty/windows/mod.rs
+++ ghidra-rs/src/pty/windows/mod.rs
@@ -3,9 +3,11 @@ pub mod handle_input_stream;
 pub mod handle_output_stream;
 pub mod jna;
 pub mod pipe;
+pub mod pseudo_console_handle;
 
 pub use handle::Handle;
 pub use handle::RawHandle;
 pub use handle_input_stream::HandleInputStream;
 pub use handle_output_stream::HandleOutputStream;
 pub use pipe::Pipe;
+pub use pseudo_console_handle::PseudoConsoleHandle;
