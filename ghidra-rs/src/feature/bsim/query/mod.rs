664ee29adc462130367d5442325f3b518856cba7
Josh Madden <cyrex562@gmail.com>
2026-07-01 23:27:35 -0400

    port: MinimalErrorLogger [tests:SKIP] (orig_src/Ghidra/Features/BSim/src/main/java/ghidra/features/bsim/query/MinimalErrorLogger.java)

--- ghidra-rs/src/feature/bsim/query/mod.rs
+++ ghidra-rs/src/feature/bsim/query/mod.rs
@@ -3,6 +3,8 @@ pub mod description;
 pub mod elastic;
 pub mod facade;
 pub mod lsh_exception;
+pub mod minimal_error_logger;
 pub mod postgresql;
 
 pub use lsh_exception::LshException;
+pub use minimal_error_logger::MinimalErrorLogger;
