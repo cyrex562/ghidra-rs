10219ea5d5ad60bd083a4dfff2387bc39f858c2d
Josh Madden <cyrex562@gmail.com>
2026-07-02 09:15:04 -0400

    port: ByteBlockRange [tests:SKIP] (orig_src/Ghidra/Features/ByteViewer/src/main/java/ghidra/app/plugin/core/format/ByteBlockRange.java)

--- ghidra-rs/src/app/plugin/core/format/mod.rs
+++ ghidra-rs/src/app/plugin/core/format/mod.rs
@@ -1,7 +1,9 @@
 pub mod byte_block;
 pub mod byte_block_access_exception;
 pub mod byte_block_info;
+pub mod byte_block_range;
 
 pub use byte_block::ByteBlock;
 pub use byte_block_access_exception::ByteBlockAccessException;
 pub use byte_block_info::ByteBlockInfo;
+pub use byte_block_range::ByteBlockRange;
