6d7675eb1ed348e7389ea5741b87ee3feac7c123
Josh Madden <cyrex562@gmail.com>
2026-07-02 04:00:03 -0400

    port: Rectangle2D [tests:SKIP] (orig_src/Ghidra/Debug/ProposedUtils/src/main/java/ghidra/util/database/spatial/rect/Rectangle2D.java)

--- ghidra-rs/src/util/database/spatial/rect/mod.rs
+++ ghidra-rs/src/util/database/spatial/rect/mod.rs
@@ -2,8 +2,10 @@ pub mod euclidean_space2d;
 pub mod immutable_point_2d;
 pub mod immutable_rectangle2d;
 pub mod point2d;
+pub mod rectangle2d;
 
 pub use euclidean_space2d::EuclideanSpace2D;
 pub use immutable_point_2d::ImmutablePoint2D;
 pub use immutable_rectangle2d::ImmutableRectangle2D;
 pub use point2d::Point2D;
+pub use rectangle2d::{encloses, Rectangle2D};
