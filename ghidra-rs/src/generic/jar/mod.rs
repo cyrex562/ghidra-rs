077d4e3d19cc7826db145995b1915df465642481
Josh Madden <cyrex562@gmail.com>
2026-07-02 22:03:28 -0400

    port: JarArchiveBuilder [tests:SKIP] (orig_src/Ghidra/Framework/Generic/src/main/java/generic/util/JarArchiveBuilder.java)

--- ghidra-rs/src/generic/jar/mod.rs
+++ ghidra-rs/src/generic/jar/mod.rs
@@ -4,6 +4,7 @@ pub mod jar_entry_filter;
 pub mod jar_entry_node;
 pub mod jar_entry_root_node;
 pub mod g_class_loader;
+pub mod jar_archive_builder;
 
 pub use resource::{FileResource, Resource};
 pub use resource_file::ResourceFile;
@@ -11,3 +12,4 @@ pub use jar_entry_filter::{JarEntry, JarEntryFilter};
 pub use jar_entry_node::{JarEntryNode, JarFileAccess, NodeRef};
 pub use jar_entry_root_node::{DefaultFilter, JarEntryRootNode};
 pub use g_class_loader::GClassLoader;
+pub use jar_archive_builder::JarArchiveBuilder;
