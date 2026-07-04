ee082e14b3018f044a6bc966c1dae463cfb31b22
Josh Madden <cyrex562@gmail.com>
2026-07-04 01:51:35 -0400

    port: ListingDisplaySearchAddressIterator [tests:SKIP] (orig_src/Ghidra/Features/Base/src/main/java/ghidra/app/plugin/core/searchtext/ListingDisplaySearchAddressIterator.java)

--- ghidra-rs/src/app/plugin/core/searchtext/mod.rs
+++ ghidra-rs/src/app/plugin/core/searchtext/mod.rs
@@ -1,5 +1,7 @@
 pub mod iterators;
+pub mod listing_display_search_address_iterator;
 pub mod search_options;
 
 pub use iterators::SearchAddressIterator;
+pub use listing_display_search_address_iterator::ListingDisplaySearchAddressIterator;
 pub use search_options::SearchOptions;
