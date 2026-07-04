1dba3233d2b0f12f6f95d9c4ba16710b1ff4844d
Josh Madden <cyrex562@gmail.com>
2026-07-03 22:19:27 -0400

    port: SearchAddressIterator [tests:SKIP] (orig_src/Ghidra/Features/Base/src/main/java/ghidra/app/plugin/core/searchtext/iterators/SearchAddressIterator.java)

---
+++ ghidra-rs/src/app/plugin/core/searchtext/iterators/mod.rs
@@ -0,0 +1,3 @@
+pub mod search_address_iterator;
+
+pub use search_address_iterator::SearchAddressIterator;
--- ghidra-rs/src/app/plugin/core/searchtext/mod.rs
+++ ghidra-rs/src/app/plugin/core/searchtext/mod.rs
@@ -1,3 +1,5 @@
+pub mod iterators;
 pub mod search_options;
 
+pub use iterators::SearchAddressIterator;
 pub use search_options::SearchOptions;
