22e5a514b49f6e8d7d85b58a2e65aa1099cc14f1
Josh Madden <cyrex562@gmail.com>
2026-07-03 23:58:04 -0400

    port: CommentSearchAddressIterator [tests:SKIP] (orig_src/Ghidra/Features/Base/src/main/java/ghidra/app/plugin/core/searchtext/iterators/CommentSearchAddressIterator.java)

--- ghidra-rs/src/app/plugin/core/searchtext/iterators/mod.rs
+++ ghidra-rs/src/app/plugin/core/searchtext/iterators/mod.rs
@@ -1,3 +1,5 @@
 pub mod search_address_iterator;
+pub mod comment_search_address_iterator;
 
 pub use search_address_iterator::SearchAddressIterator;
+pub use comment_search_address_iterator::CommentSearchAddressIterator;
