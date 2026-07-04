a4c2f06a2e423eb684543f3341d616b169472f7c
Josh Madden <cyrex562@gmail.com>
2026-07-04 00:58:02 -0400

    port: DBKeyAdapter [tests:SKIP] (orig_src/Ghidra/Framework/SoftwareModeling/src/main/java/ghidra/program/database/util/DBKeyAdapter.java)

--- ghidra-rs/src/program/util/mod.rs
+++ ghidra-rs/src/program/util/mod.rs
@@ -1,4 +1,5 @@
 pub mod code_unit_insertion_exception;
+pub mod db_key_adapter;
 pub mod deleted_exception;
 pub mod multi_address_iterator;
 pub mod processor_symbol_type;
@@ -6,6 +7,7 @@ pub mod program_conflict_exception;
 pub mod program_diff_filter;
 
 pub use code_unit_insertion_exception::CodeUnitInsertionException;
+pub use db_key_adapter::DBKeyAdapter;
 pub use deleted_exception::DeletedException;
 pub use multi_address_iterator::MultiAddressIterator;
 pub use processor_symbol_type::ProcessorSymbolType;
