e138cc69d71893b1276ae554fa40c7eaf43ef9a2
Josh Madden <cyrex562@gmail.com>
2026-07-04 00:45:29 -0400

    port: MessageDigestFactory [tests:SKIP] (orig_src/Ghidra/Framework/Generic/src/main/java/generic/hash/MessageDigestFactory.java)

--- ghidra-rs/src/generic/hash/mod.rs
+++ ghidra-rs/src/generic/hash/mod.rs
@@ -1,5 +1,7 @@
 pub mod simple_crc32;
 pub mod message_digest;
+pub mod message_digest_factory;
 
 pub use simple_crc32::{SimpleCRC32, CRC32_TABLE};
 pub use message_digest::MessageDigest;
+pub use message_digest_factory::MessageDigestFactory;
