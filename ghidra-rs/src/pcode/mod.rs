e5c729486d6d652882926de2b2d4fd6d64706dc2
Josh Madden <cyrex562@gmail.com>
2026-07-03 23:16:39 -0400

    port: LoadImage [tests:SKIP] (orig_src/Ghidra/Framework/Emulation/src/main/java/ghidra/pcode/loadimage/LoadImage.java)

--- ghidra-rs/src/pcode/mod.rs
+++ ghidra-rs/src/pcode/mod.rs
@@ -2,7 +2,10 @@ pub mod emu;
 pub mod emulate;
 pub mod error;
 pub mod floatformat;
+pub mod load_image;
 pub mod memstate;
 pub mod opbehavior;
 pub mod r#struct;
 pub mod utils;
+
+pub use load_image::LoadImage;
