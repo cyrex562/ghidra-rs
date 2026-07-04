081bae44da185578e99a23361bd10e67e3d78b3c
Josh Madden <cyrex562@gmail.com>
2026-07-04 00:14:04 -0400

    port: AddressMatch [tests:SKIP] (orig_src/Ghidra/Features/Base/src/main/java/ghidra/util/bytesearch/AddressMatch.java)

--- ghidra-rs/src/util/bytesearch/mod.rs
+++ ghidra-rs/src/util/bytesearch/mod.rs
@@ -4,6 +4,7 @@ pub mod ditted_bit_sequence;
 pub mod extended_byte_sequence;
 pub mod input_stream_buffer_byte_sequence;
 pub mod r#match;
+pub mod address_match;
 
 pub use byte_pattern::BytePattern;
 pub use byte_sequence::ByteSequence;
@@ -11,3 +12,4 @@ pub use ditted_bit_sequence::DittedBitSequence;
 pub use extended_byte_sequence::ExtendedByteSequence;
 pub use input_stream_buffer_byte_sequence::InputStreamBufferByteSequence;
 pub use r#match::Match;
+pub use address_match::AddressMatch;
