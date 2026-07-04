03c2c9689f8abee47e8455064181765fadf65fa8
Josh Madden <cyrex562@gmail.com>
2026-07-04 00:28:42 -0400

    port: ExtFragmentRange [tests:SKIP] (orig_src/Ghidra/Features/Sarif/src/main/java/sarif/export/trees/ExtFragmentRange.java)

--- ghidra-rs/src/sarif/export/mod.rs
+++ ghidra-rs/src/sarif/export/mod.rs
@@ -3,9 +3,11 @@ pub mod code;
 pub mod ep;
 pub mod extlib;
 pub mod relocs;
+pub mod trees;
 
 pub use bkmk::ExtBookmark;
 pub use code::ExtCodeBlock;
 pub use ep::ExtEntryPoint;
 pub use extlib::ExtLibrary;
 pub use relocs::ExtRelocation;
+pub use trees::ExtFragmentRange;
---
+++ ghidra-rs/src/sarif/export/trees/mod.rs
@@ -0,0 +1,3 @@
+pub mod ext_fragment_range;
+
+pub use ext_fragment_range::ExtFragmentRange;
