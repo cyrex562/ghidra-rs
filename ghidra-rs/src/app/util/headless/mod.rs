7d38704b6a933b1413074f7b49cea2a9b7e79b90
Josh Madden <cyrex562@gmail.com>
2026-07-04 01:55:44 -0400

    port: HeadlessTimedTaskMonitor [tests:SKIP] (orig_src/Ghidra/Features/Base/src/main/java/ghidra/app/util/headless/HeadlessTimedTaskMonitor.java)

--- ghidra-rs/src/app/util/headless/mod.rs
+++ ghidra-rs/src/app/util/headless/mod.rs
@@ -1,3 +1,5 @@
 pub mod headless_error_logger;
+pub mod headless_timed_task_monitor;
 
 pub use headless_error_logger::HeadlessErrorLogger;
+pub use headless_timed_task_monitor::HeadlessTimedTaskMonitor;
