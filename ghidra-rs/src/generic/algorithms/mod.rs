54ec0313180514e6321a0a5b03f7a269dbbba24c
Josh Madden <cyrex562@gmail.com>
2026-07-04 22:03:55 -0400

    port: StringReducingLcs [tests:SKIP] (orig_src/Ghidra/Framework/Generic/src/main/java/generic/algorithms/StringReducingLcs.java)

--- ghidra-rs/src/generic/algorithms/mod.rs
+++ ghidra-rs/src/generic/algorithms/mod.rs
@@ -2,6 +2,7 @@ pub mod crc64;
 pub mod lcs;
 pub mod reducing_lcs;
 pub mod reducing_list_based_lcs;
+pub mod string_reducing_lcs;
 pub mod word_differ;
 
 pub use crate::util::task::{DummyMonitor, TaskMonitor};
@@ -9,4 +10,5 @@ pub use crc64::CRC64;
 pub use lcs::{get_reducing_lcs, LcsTrait};
 pub use reducing_lcs::{ReducingLcs, ReducingLcsOps};
 pub use reducing_list_based_lcs::ReducingListBasedLcs;
+pub use string_reducing_lcs::StringReducingLcs;
 pub use word_differ::{WordDiffer, WordPart};
