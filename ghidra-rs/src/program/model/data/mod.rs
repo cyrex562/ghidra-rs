e074d2fe5963df1de9ad0d121e8356a5908b1091
Josh Madden <cyrex562@gmail.com>
2026-07-04 20:19:05 -0400

    seam: DataTypeTransferable -> trait (orig_src/Ghidra/Framework/SoftwareModeling/src/main/java/ghidra/program/model/data/DataTypeTransferable.java)

--- ghidra-rs/src/program/model/data/mod.rs
+++ ghidra-rs/src/program/model/data/mod.rs
@@ -9,6 +9,7 @@ pub mod data_organization;
 pub mod data_type_dependency_exception;
 pub mod data_type_display_options;
 pub mod data_type_manager_domain_object;
+pub mod data_type_transferable;
 pub mod enum_;
 pub mod enum_value_partitioner;
 pub mod file_archive_based_data_type_manager;
