7073a82029f9aa85f629eefc72ce865d56117f6d
Josh Madden <cyrex562@gmail.com>
2026-07-04 22:36:43 -0400

    port: AbstractMessageDigest [tests:SKIP] (orig_src/Ghidra/Framework/Generic/src/main/java/generic/hash/AbstractMessageDigest.java)

--- ghidra-rs/src/generic/hash/mod.rs
+++ ghidra-rs/src/generic/hash/mod.rs
@@ -1,7 +1,9 @@
 pub mod simple_crc32;
 pub mod message_digest;
 pub mod message_digest_factory;
+pub mod abstract_message_digest;
 
 pub use simple_crc32::{SimpleCRC32, CRC32_TABLE};
 pub use message_digest::MessageDigest;
 pub use message_digest_factory::MessageDigestFactory;
+pub use abstract_message_digest::{AbstractMessageDigest, MessageDigestBehavior};
