2f314b57ce53ebf5f9ddb6d0ba3b84fa5afb9c98
Josh Madden <cyrex562@gmail.com>
2026-07-05 00:05:16 -0400

    port: MergeUtilities [tests:SKIP] (orig_src/Ghidra/Features/Base/src/main/java/ghidra/app/merge/util/MergeUtilities.java)

--- ghidra-rs/src/app/merge/mod.rs
+++ ghidra-rs/src/app/merge/mod.rs
@@ -3,7 +3,9 @@ pub mod merge_progress_modifier;
 pub mod merge_resolver;
 pub mod program_specific_address_translator;
 pub mod structures;
+pub mod util;
 
 pub use merge_progress_modifier::MergeProgressModifier;
 pub use merge_resolver::MergeResolver;
 pub use program_specific_address_translator::ProgramSpecificAddressTranslator;
+pub use util::MergeUtilities;
