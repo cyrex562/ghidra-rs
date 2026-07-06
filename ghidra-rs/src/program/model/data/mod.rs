017ec11f643d3345193d2db454cf54158aec9d43
Josh Madden <cyrex562@gmail.com>
2026-07-05 20:17:20 -0400

    seam: Dynamic -> trait (orig_src/Ghidra/Framework/SoftwareModeling/src/main/java/ghidra/program/model/data/Dynamic.java)

--- ghidra-rs/src/program/model/data/mod.rs
+++ ghidra-rs/src/program/model/data/mod.rs
@@ -22,6 +22,7 @@ pub mod data_type_manager_domain_object;
 pub mod data_type_transferable;
 pub mod data_type_with_charset;
 pub mod domain_file_based_data_type_manager;
+pub mod dynamic;
 pub mod enum_;
 pub mod enum_value_partitioner;
 pub mod factory_data_type;
