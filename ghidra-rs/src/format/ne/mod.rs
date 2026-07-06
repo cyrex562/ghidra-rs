3f72d10b71b0938f5d9bc3b04ae573ed6d0e729a
Josh Madden <cyrex562@gmail.com>
2026-07-05 22:31:46 -0400

    port: RelocationInternalRef [tests:SKIP] (orig_src/Ghidra/Features/Base/src/main/java/ghidra/app/util/bin/format/ne/RelocationInternalRef.java)

--- ghidra-rs/src/format/ne/mod.rs
+++ ghidra-rs/src/format/ne/mod.rs
@@ -2,8 +2,10 @@ pub mod invalid_windows_header_exception;
 pub mod length_string_set;
 pub mod relocation_imported_name;
 pub mod relocation_imported_ordinal;
+pub mod relocation_internal_ref;
 
 pub use invalid_windows_header_exception::InvalidWindowsHeaderException;
 pub use length_string_set::LengthStringSet;
 pub use relocation_imported_name::RelocationImportedName;
 pub use relocation_imported_ordinal::RelocationImportedOrdinal;
+pub use relocation_internal_ref::RelocationInternalRef;
