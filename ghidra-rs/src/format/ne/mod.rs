b4aff09b3de655747fab1744ee6f33d2fa30e659
Josh Madden <cyrex562@gmail.com>
2026-07-05 22:26:43 -0400

    port: RelocationImportedName [tests:SKIP] (orig_src/Ghidra/Features/Base/src/main/java/ghidra/app/util/bin/format/ne/RelocationImportedName.java)

--- ghidra-rs/src/format/ne/mod.rs
+++ ghidra-rs/src/format/ne/mod.rs
@@ -1,5 +1,7 @@
 pub mod invalid_windows_header_exception;
 pub mod length_string_set;
+pub mod relocation_imported_name;
 
 pub use invalid_windows_header_exception::InvalidWindowsHeaderException;
 pub use length_string_set::LengthStringSet;
+pub use relocation_imported_name::RelocationImportedName;
