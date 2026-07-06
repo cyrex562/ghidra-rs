131a29ef7b7211dd31220238526751ae0f5665ba
Josh Madden <cyrex562@gmail.com>
2026-07-05 23:42:26 -0400

    port: InstructionPanelListener [tests:SKIP] (orig_src/Ghidra/Features/Base/src/main/java/ghidra/app/plugin/core/references/InstructionPanelListener.java)

--- ghidra-rs/src/app/plugin/core/references/mod.rs
+++ ghidra-rs/src/app/plugin/core/references/mod.rs
@@ -1,3 +1,5 @@
+pub mod instruction_panel_listener;
 pub mod reserved_name_exception;
 
+pub use instruction_panel_listener::InstructionPanelListener;
 pub use reserved_name_exception::ReservedNameException;
