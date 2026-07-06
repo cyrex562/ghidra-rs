f7f2fd69e64fae61c16612f0ff97fc8d688a3b08
Josh Madden <cyrex562@gmail.com>
2026-07-06 01:31:40 -0400

    port: TraceChangeSet [tests:SKIP] (orig_src/Ghidra/Debug/Framework-TraceModeling/src/main/java/ghidra/trace/model/TraceChangeSet.java)

--- ghidra-rs/src/trace/model/mod.rs
+++ ghidra-rs/src/trace/model/mod.rs
@@ -4,6 +4,7 @@ pub mod default_address_snap;
 pub mod memory;
 pub mod target;
 pub mod time;
+pub mod trace_change_set;
 pub mod trace_closed_exception;
 pub mod trace_execution_state;
 pub mod trace_span;
