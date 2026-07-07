2b5ccd26d9bedcedff6699cd03f5c8aacefc1d07
Josh Madden <cyrex562@gmail.com>
2026-07-07 03:25:19 -0400

    port: ExtModule [tests:SKIP] (orig_src/Ghidra/Features/Sarif/src/main/java/sarif/export/trees/ExtModule.java)

--- ghidra-rs/src/sarif/export/trees/mod.rs
+++ ghidra-rs/src/sarif/export/trees/mod.rs
@@ -1,5 +1,7 @@
 pub mod ext_fragment;
 pub mod ext_fragment_range;
+pub mod ext_module;
 
 pub use ext_fragment::ExtFragment;
 pub use ext_fragment_range::ExtFragmentRange;
+pub use ext_module::ExtModule;
