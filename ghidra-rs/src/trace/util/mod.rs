eb778697eb0e7e6efe20fdd69e7f3725e324a094
Josh Madden <cyrex562@gmail.com>
2026-07-07 01:22:47 -0400

    port: DataAdapterFromSettings [tests:SKIP] (orig_src/Ghidra/Debug/Framework-TraceModeling/src/main/java/ghidra/trace/util/DataAdapterFromSettings.java)

--- ghidra-rs/src/trace/util/mod.rs
+++ ghidra-rs/src/trace/util/mod.rs
@@ -1,5 +1,6 @@
 pub mod byte_array_utils;
 pub mod copy_on_write;
+pub mod data_adapter_from_settings;
 pub mod data_adapter_minimal;
 pub mod empty_function_iterator;
 pub mod enumerating_iterator;
