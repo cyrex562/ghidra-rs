fec65ed8ac212b821f67a6bed80ae5dcaf02927e
Josh Madden <cyrex562@gmail.com>
2026-07-07 01:25:25 -0400

    port: SetFunctionRepeatableCommentCmd [tests:SKIP] (orig_src/Ghidra/Features/Base/src/main/java/ghidra/app/cmd/function/SetFunctionRepeatableCommentCmd.java)

--- ghidra-rs/src/app/cmd/function/mod.rs
+++ ghidra-rs/src/app/cmd/function/mod.rs
@@ -1,5 +1,7 @@
+pub mod set_function_repeatable_comment_cmd;
 pub mod set_function_var_args_command;
 pub mod set_variable_comment_cmd;
 
+pub use set_function_repeatable_comment_cmd::SetFunctionRepeatableCommentCmd;
 pub use set_function_var_args_command::SetFunctionVarArgsCommand;
 pub use set_variable_comment_cmd::SetVariableCommentCmd;
