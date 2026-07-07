5da6ab4e5905ccfe14b7aef9a18e64f38e9f0206
Josh Madden <cyrex562@gmail.com>
2026-07-06 23:19:55 -0400

    port: CodeUnitCountSettingsDefinition [tests:SKIP] (orig_src/Ghidra/Features/Base/src/main/java/ghidra/util/table/field/CodeUnitCountSettingsDefinition.java)

--- ghidra-rs/src/util/table/field/mod.rs
+++ ghidra-rs/src/util/table/field/mod.rs
@@ -1,9 +1,11 @@
 pub mod byte_count_settings_definition;
+pub mod code_unit_count_settings_definition;
 pub mod function_inline_settings_definition;
 pub mod function_no_return_settings_definition;
 pub mod function_thunk_settings_definition;
 
 pub use byte_count_settings_definition::ByteCountSettingsDefinition;
+pub use code_unit_count_settings_definition::CodeUnitCountSettingsDefinition;
 pub use function_inline_settings_definition::FunctionInlineSettingsDefinition;
 pub use function_no_return_settings_definition::FunctionNoReturnSettingsDefinition;
 pub use function_thunk_settings_definition::FunctionThunkSettingsDefinition;
