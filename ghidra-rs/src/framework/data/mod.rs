cc065e125239547a16ac2162d887f50f66c73e8d
Josh Madden <cyrex562@gmail.com>
2026-07-07 01:53:40 -0400

    port: OpenedDomainFile [tests:SKIP] (orig_src/Ghidra/Debug/ProposedUtils/src/main/java/ghidra/framework/data/OpenedDomainFile.java)

--- ghidra-rs/src/framework/data/mod.rs
+++ ghidra-rs/src/framework/data/mod.rs
@@ -2,8 +2,10 @@ pub mod checkin_handler;
 pub mod default_checkin_handler;
 pub mod domain_object_file_listener;
 pub mod open_mode;
+pub mod opened_domain_file;
 
 pub use checkin_handler::CheckinHandler;
 pub use default_checkin_handler::DefaultCheckinHandler;
 pub use domain_object_file_listener::DomainObjectFileListener;
 pub use open_mode::OpenMode;
+pub use opened_domain_file::{OpenedDomainFile, OpenedDomainFileError};
