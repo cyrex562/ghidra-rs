c1270def5f19e5d08c89d62dbe8b539c84a6d4a5
Josh Madden <cyrex562@gmail.com>
2026-07-07 02:23:50 -0400

    port: IsfEnum [tests:SKIP] (orig_src/Ghidra/Debug/Debugger-isf/src/main/java/ghidra/program/model/data/ISF/IsfEnum.java)

--- ghidra-rs/src/program/model/data/isf/mod.rs
+++ ghidra-rs/src/program/model/data/isf/mod.rs
@@ -9,6 +9,7 @@ pub mod isf_win_pe;
 pub mod abstract_isf_writer;
 pub mod isf_producer;
 pub mod abstract_isf_object;
+pub mod isf_enum;
 
 pub use isf_object::IsfObject;
 pub use isf_data_type_null::IsfDataTypeNull;
@@ -21,3 +22,4 @@ pub use isf_win_pe::IsfWinPE;
 pub use abstract_isf_writer::{IsfWriterImpl, AbstractIsfWriterState};
 pub use isf_producer::IsfProducer;
 pub use abstract_isf_object::AbstractIsfObject;
+pub use isf_enum::IsfEnum;
