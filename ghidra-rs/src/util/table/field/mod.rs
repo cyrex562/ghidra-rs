a6d361b89706dccf23d39f1b8f870ed1beeaae7a
Josh Madden <cyrex562@gmail.com>
2026-07-06 23:21:33 -0400

    port: CodeUnitOffsetSettingsDefinition [tests:SKIP] (orig_src/Ghidra/Features/Base/src/main/java/ghidra/util/table/field/CodeUnitOffsetSettingsDefinition.java)

--- ghidra-rs/src/util/table/field/mod.rs
+++ ghidra-rs/src/util/table/field/mod.rs
@@ -1,11 +1,13 @@
 pub mod byte_count_settings_definition;
 pub mod code_unit_count_settings_definition;
+pub mod code_unit_offset_settings_definition;
 pub mod function_inline_settings_definition;
 pub mod function_no_return_settings_definition;
 pub mod function_thunk_settings_definition;
 
 pub use byte_count_settings_definition::ByteCountSettingsDefinition;
 pub use code_unit_count_settings_definition::CodeUnitCountSettingsDefinition;
+pub use code_unit_offset_settings_definition::CodeUnitOffsetSettingsDefinition;
 pub use function_inline_settings_definition::FunctionInlineSettingsDefinition;
 pub use function_no_return_settings_definition::FunctionNoReturnSettingsDefinition;
 pub use function_thunk_settings_definition::FunctionThunkSettingsDefinition;
