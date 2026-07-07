aadd825f7a5105df7e4475c092c1b63a29421320
Josh Madden <cyrex562@gmail.com>
2026-07-07 02:52:28 -0400

    port: SetReturnDataTypeCmd [tests:SKIP] (orig_src/Ghidra/Features/Base/src/main/java/ghidra/app/cmd/function/SetReturnDataTypeCmd.java)

--- ghidra-rs/src/app/cmd/function/mod.rs
+++ ghidra-rs/src/app/cmd/function/mod.rs
@@ -1,9 +1,11 @@
 pub mod set_function_name_cmd;
 pub mod set_function_repeatable_comment_cmd;
 pub mod set_function_var_args_command;
+pub mod set_return_data_type_cmd;
 pub mod set_variable_comment_cmd;
 
 pub use set_function_name_cmd::SetFunctionNameCmd;
 pub use set_function_repeatable_comment_cmd::SetFunctionRepeatableCommentCmd;
 pub use set_function_var_args_command::SetFunctionVarArgsCommand;
+pub use set_return_data_type_cmd::SetReturnDataTypeCmd;
 pub use set_variable_comment_cmd::SetVariableCommentCmd;
