f7285373425a3bb23a5029a4b03c814e4a41379c
Josh Madden <cyrex562@gmail.com>
2026-07-07 00:40:34 -0400

    port: DefaultCheckinHandler [tests:SKIP] (orig_src/Ghidra/Framework/Project/src/main/java/ghidra/framework/data/DefaultCheckinHandler.java)

--- ghidra-rs/src/framework/data/mod.rs
+++ ghidra-rs/src/framework/data/mod.rs
@@ -1,7 +1,9 @@
 pub mod checkin_handler;
+pub mod default_checkin_handler;
 pub mod domain_object_file_listener;
 pub mod open_mode;
 
 pub use checkin_handler::CheckinHandler;
+pub use default_checkin_handler::DefaultCheckinHandler;
 pub use domain_object_file_listener::DomainObjectFileListener;
 pub use open_mode::OpenMode;
