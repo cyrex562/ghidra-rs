6283eddf46c444630d52621381f63c923cdaa1e5
Josh Madden <cyrex562@gmail.com>
2026-07-06 23:02:25 -0400

    port: ExtStackReference [tests:SKIP] (orig_src/Ghidra/Features/Sarif/src/main/java/sarif/export/ref/ExtStackReference.java)

--- ghidra-rs/src/sarif/export/ref/mod.rs
+++ ghidra-rs/src/sarif/export/ref/mod.rs
@@ -1,7 +1,9 @@
 pub mod ext_reference;
 pub mod ext_register_reference;
 pub mod ext_shifted_reference;
+pub mod ext_stack_reference;
 
 pub use ext_reference::ExtReference;
 pub use ext_register_reference::ExtRegisterReference;
 pub use ext_shifted_reference::ExtShiftedReference;
+pub use ext_stack_reference::ExtStackReference;
