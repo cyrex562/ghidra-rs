68dc97b4e035af7ad127cfd59a594500b3554a38
Josh Madden <cyrex562@gmail.com>
2026-07-07 01:32:17 -0400

    port: ClearFallThroughCmd [tests:SKIP] (orig_src/Ghidra/Features/Base/src/main/java/ghidra/app/cmd/refs/ClearFallThroughCmd.java)

--- ghidra-rs/src/app/cmd/mod.rs
+++ ghidra-rs/src/app/cmd/mod.rs
@@ -2,3 +2,4 @@ pub mod data;
 pub mod function;
 pub mod memory;
 pub mod module;
+pub mod refs;
---
+++ ghidra-rs/src/app/cmd/refs/mod.rs
@@ -0,0 +1,3 @@
+pub mod clear_fall_through_cmd;
+
+pub use clear_fall_through_cmd::ClearFallThroughCmd;
