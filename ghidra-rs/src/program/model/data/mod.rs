ed336261ea4b18e91eac2c0a3a4d6f675473468f
Josh Madden <cyrex562@gmail.com>
2026-07-06 23:46:10 -0400

    port: PaddingSettingsDefinition [tests:SKIP] (orig_src/Ghidra/Framework/SoftwareModeling/src/main/java/ghidra/program/model/data/PaddingSettingsDefinition.java)

--- ghidra-rs/src/program/model/data/mod.rs
+++ ghidra-rs/src/program/model/data/mod.rs
@@ -46,6 +46,7 @@ pub mod leb128;
 pub mod mem_buffer_image_input_stream;
 pub mod mutability_settings_definition;
 pub mod packing_type;
+pub mod padding_settings_definition;
 pub mod parameter_definition;
 pub mod playable;
 pub mod pointer;
