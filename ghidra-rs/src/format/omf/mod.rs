752f65a072fda186732b76b65c34ebaf3cb52386
Josh Madden <cyrex562@gmail.com>
2026-07-08 02:55:34 -0400

    port: AbstractOmfRecordFactory [tests:SKIP] (orig_src/Ghidra/Features/Base/src/main/java/ghidra/app/util/bin/format/omf/AbstractOmfRecordFactory.java)

--- ghidra-rs/src/format/omf/mod.rs
+++ ghidra-rs/src/format/omf/mod.rs
@@ -1,9 +1,11 @@
+pub mod abstract_omf_record_factory;
 pub mod omf51;
 pub mod omf_exception;
 pub mod omf_index;
 pub mod omf_record;
 pub mod omf_symbol;
 
+pub use abstract_omf_record_factory::AbstractOmfRecordFactory;
 pub use omf_exception::OmfException;
 pub use omf_index::OmfIndex;
 pub use omf_record::OmfRecord;
