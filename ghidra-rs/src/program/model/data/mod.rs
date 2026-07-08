674da15d4c7c5c95dad6a910e94863a1d41fe404
Josh Madden <cyrex562@gmail.com>
2026-07-08 00:46:57 -0400

    port: GenericCallingConvention [tests:SKIP] (orig_src/Ghidra/Framework/SoftwareModeling/src/main/java/ghidra/program/model/data/GenericCallingConvention.java)

--- ghidra-rs/src/program/model/data/mod.rs
+++ ghidra-rs/src/program/model/data/mod.rs
@@ -39,6 +39,7 @@ pub mod factory_data_type;
 pub mod file_archive_based_data_type_manager;
 pub mod file_based_data_type_manager;
 pub mod function_definition;
+pub mod generic_calling_convention;
 pub mod icategory;
 pub mod illegal_rename_exception;
 pub mod invalid_data_type_exception;
