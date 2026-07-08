fe429b5c3b7ad5cc19e2bcf583132ed2f9472d60
Josh Madden <cyrex562@gmail.com>
2026-07-08 01:51:44 -0400

    port: OffsetShiftSettingsDefinition [tests:SKIP] (orig_src/Ghidra/Framework/SoftwareModeling/src/main/java/ghidra/program/model/data/OffsetShiftSettingsDefinition.java)

--- ghidra-rs/src/program/model/data/mod.rs
+++ ghidra-rs/src/program/model/data/mod.rs
@@ -51,6 +51,7 @@ pub mod leb128;
 pub mod mem_buffer_image_input_stream;
 pub mod mutability_settings_definition;
 pub mod offset_mask_settings_definition;
+pub mod offset_shift_settings_definition;
 pub mod packing_type;
 pub mod padding_settings_definition;
 pub mod parameter_definition;
