2a6dbb8e3dd10a374806fd8318d1d5355f8f592f
Josh Madden <cyrex562@gmail.com>
2026-07-08 01:49:49 -0400

    port: OffsetMaskSettingsDefinition [tests:SKIP] (orig_src/Ghidra/Framework/SoftwareModeling/src/main/java/ghidra/program/model/data/OffsetMaskSettingsDefinition.java)

--- ghidra-rs/src/program/model/data/mod.rs
+++ ghidra-rs/src/program/model/data/mod.rs
@@ -50,6 +50,7 @@ pub mod invalidated_listener;
 pub mod leb128;
 pub mod mem_buffer_image_input_stream;
 pub mod mutability_settings_definition;
+pub mod offset_mask_settings_definition;
 pub mod packing_type;
 pub mod padding_settings_definition;
 pub mod parameter_definition;
