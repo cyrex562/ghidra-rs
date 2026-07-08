bbf64f6f71439de104438e26cc5af1a7abb70b54
Josh Madden <cyrex562@gmail.com>
2026-07-08 03:39:19 -0400

    port: Omf2or4 [tests:SKIP] (orig_src/Ghidra/Features/Base/src/main/java/ghidra/app/util/bin/format/omf/Omf2or4.java)

--- ghidra-rs/src/format/omf/mod.rs
+++ ghidra-rs/src/format/omf/mod.rs
@@ -1,4 +1,5 @@
 pub mod abstract_omf_record_factory;
+pub mod omf2or4;
 pub mod omf51;
 pub mod omf_exception;
 pub mod omf_index;
@@ -6,6 +7,7 @@ pub mod omf_record;
 pub mod omf_symbol;
 
 pub use abstract_omf_record_factory::AbstractOmfRecordFactory;
+pub use omf2or4::Omf2or4;
 pub use omf_exception::OmfException;
 pub use omf_index::OmfIndex;
 pub use omf_record::OmfRecord;
