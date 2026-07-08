b1a0d7dbd2e354b081f12d002c95303bd2a418a8
Josh Madden <cyrex562@gmail.com>
2026-07-07 22:31:05 -0400

    port: DexConstants [tests:SKIP] (orig_src/Ghidra/Features/FileFormats/src/main/java/ghidra/file/formats/android/dex/format/DexConstants.java)

--- ghidra-rs/src/file/formats/android/dex/format/mod.rs
+++ ghidra-rs/src/file/formats/android/dex/format/mod.rs
@@ -2,6 +2,7 @@ pub mod access_flags;
 pub mod debug_info_opcodes;
 pub(crate) mod debug_info_state_machine_reader;
 pub mod debug_state_machine_op_codes;
+pub mod dex_constants;
 pub mod map_item_type;
 pub mod map_item_type_codes;
 pub mod method_handle_type;
