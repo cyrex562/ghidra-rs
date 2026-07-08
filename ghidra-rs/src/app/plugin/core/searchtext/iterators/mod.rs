3065f2d4c641ba238cfdf380f17d63d93b85c988
Josh Madden <cyrex562@gmail.com>
2026-07-08 01:59:13 -0400

    port: LabelSearchAddressIterator [tests:SKIP] (orig_src/Ghidra/Features/Base/src/main/java/ghidra/app/plugin/core/searchtext/iterators/LabelSearchAddressIterator.java)

--- ghidra-rs/src/app/plugin/core/searchtext/iterators/mod.rs
+++ ghidra-rs/src/app/plugin/core/searchtext/iterators/mod.rs
@@ -2,8 +2,10 @@ pub mod search_address_iterator;
 pub mod comment_search_address_iterator;
 pub mod function_search_address_iterator;
 pub mod instruction_search_address_iterator;
+pub mod label_search_address_iterator;
 
 pub use search_address_iterator::SearchAddressIterator;
 pub use comment_search_address_iterator::CommentSearchAddressIterator;
 pub use function_search_address_iterator::FunctionSearchAddressIterator;
 pub use instruction_search_address_iterator::InstructionSearchAddressIterator;
+pub use label_search_address_iterator::LabelSearchAddressIterator;
