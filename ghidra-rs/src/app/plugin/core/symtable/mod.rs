5c227392e4788d94c67e57871168f5435a11fdc4
Josh Madden <cyrex562@gmail.com>
2026-07-08 04:48:05 -0400

    port: DeletedSymbolRowObject [tests:SKIP] (orig_src/Ghidra/Features/Base/src/main/java/ghidra/app/plugin/core/symtable/DeletedSymbolRowObject.java)

--- ghidra-rs/src/app/plugin/core/symtable/mod.rs
+++ ghidra-rs/src/app/plugin/core/symtable/mod.rs
@@ -1,5 +1,7 @@
+pub mod deleted_symbol_row_object;
 pub mod symbol_filter;
 pub mod symbol_row_object;
 
+pub use deleted_symbol_row_object::DeletedSymbolRowObject;
 pub use symbol_filter::SymbolFilter;
 pub use symbol_row_object::SymbolRowObject;
