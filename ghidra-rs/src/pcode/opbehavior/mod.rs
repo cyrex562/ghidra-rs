bb18fb673c37efac26cd8437fd10a008232bad76
Josh Madden <cyrex562@gmail.com>
2026-07-08 00:30:04 -0400

    port: BinaryOpBehavior [tests:SKIP] (orig_src/Ghidra/Framework/Emulation/src/main/java/ghidra/pcode/opbehavior/BinaryOpBehavior.java)

--- ghidra-rs/src/pcode/opbehavior/mod.rs
+++ ghidra-rs/src/pcode/opbehavior/mod.rs
@@ -1,5 +1,7 @@
+pub mod binary_op_behavior;
 pub mod op_behavior;
 pub mod special_op_behavior;
 
+pub use binary_op_behavior::{BinaryOpBehavior, BinaryOpBehaviorImpl};
 pub use op_behavior::OpBehavior;
 pub use special_op_behavior::{SpecialOpBehavior, special_op_behavior};
