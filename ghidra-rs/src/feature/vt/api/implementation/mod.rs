26eaa51aa75600ee0ea218ff3af465e9132d6972
Josh Madden <cyrex562@gmail.com>
2026-07-08 22:20:32 -0400

    port: VTEvent [tests:SKIP] (orig_src/Ghidra/Features/VersionTracking/src/main/java/ghidra/feature/vt/api/impl/VTEvent.java)

--- ghidra-rs/src/feature/vt/api/implementation/mod.rs
+++ ghidra-rs/src/feature/vt/api/implementation/mod.rs
@@ -1,5 +1,7 @@
 pub mod program_correlator_info_fake;
+pub mod vt_event;
 pub mod vt_program_correlator_info;
 
 pub use program_correlator_info_fake::ProgramCorrelatorInfoFake;
+pub use vt_event::VtEvent;
 pub use vt_program_correlator_info::VtProgramCorrelatorInfo;
