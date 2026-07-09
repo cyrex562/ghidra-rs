93cb3bbad7e4a4327360ff8a62ec022c42a29fed
Josh Madden <cyrex562@gmail.com>
2026-07-08 22:32:35 -0400

    port: DefaultDomainFileFilter [tests:SKIP] (orig_src/Ghidra/Framework/Project/src/main/java/ghidra/framework/model/DefaultDomainFileFilter.java)

--- ghidra-rs/src/framework/model/mod.rs
+++ ghidra-rs/src/framework/model/mod.rs
@@ -1,5 +1,6 @@
 pub mod aborted_transaction_listener;
 pub mod change_set;
+pub mod default_domain_file_filter;
 pub mod default_launch_mode;
 pub mod domain_file;
 pub mod domain_file_filter;
@@ -26,6 +27,7 @@ pub mod user_data;
 
 pub use aborted_transaction_listener::AbortedTransactionListener;
 pub use change_set::ChangeSet;
+pub use default_domain_file_filter::DefaultDomainFileFilter;
 pub use default_launch_mode::DefaultLaunchMode;
 pub use domain_file::DomainFile;
 pub use domain_file_filter::{
