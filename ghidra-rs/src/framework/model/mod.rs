204e2e737a40aa4e438d832893dff9396d6fd3c2
Josh Madden <cyrex562@gmail.com>
2026-07-08 22:48:44 -0400

    port: DomainObjectEvent [tests:SKIP] (orig_src/Ghidra/Framework/Project/src/main/java/ghidra/framework/model/DomainObjectEvent.java)

--- ghidra-rs/src/framework/model/mod.rs
+++ ghidra-rs/src/framework/model/mod.rs
@@ -9,6 +9,7 @@ pub mod domain_folder_change_listener;
 pub mod domain_folder_filter;
 pub mod domain_object;
 pub mod domain_object_closed_listener;
+pub mod domain_object_event;
 pub mod domain_object_event_id_generator;
 pub mod domain_object_exception;
 pub mod domain_object_listener;
@@ -44,6 +45,7 @@ pub use domain_folder_filter::{
 pub use domain_folder_change_listener::DomainFolderChangeListener;
 pub use domain_object::{DomainObject, DomainObjectConsumer, SaveError};
 pub use domain_object_closed_listener::DomainObjectClosedListener;
+pub use domain_object_event::DomainObjectEvent;
 pub use domain_object_event_id_generator::DomainObjectEventIdGenerator;
 pub use domain_object_exception::DomainObjectException;
 pub use domain_object_listener::DomainObjectListener;
