8e83866ee7bd02cbffafe3920b33c612c0bcb068
Josh Madden <cyrex562@gmail.com>
2026-07-09 22:53:19 -0400

    port: SleighSymbol [tests:SKIP] (orig_src/Ghidra/Framework/SoftwareModeling/src/main/java/ghidra/pcodeCPort/slghsymbol/SleighSymbol.java)

--- ghidra-rs/src/decompiler/slghsymbol/mod.rs
+++ ghidra-rs/src/decompiler/slghsymbol/mod.rs
@@ -1,5 +1,7 @@
 pub mod context_change;
+pub mod sleigh_symbol;
 pub mod symbol_type;
 
 pub use context_change::ContextChange;
+pub use sleigh_symbol::SleighSymbol;
 pub use symbol_type::SymbolType;
