2f95b3784e85935d199300be7a8d70295fafd602
Josh Madden <cyrex562@gmail.com>
2026-07-09 22:58:06 -0400

    port: LabelSymbol [tests:SKIP] (orig_src/Ghidra/Framework/SoftwareModeling/src/main/java/ghidra/pcodeCPort/slghsymbol/LabelSymbol.java)

--- ghidra-rs/src/decompiler/slghsymbol/mod.rs
+++ ghidra-rs/src/decompiler/slghsymbol/mod.rs
@@ -1,7 +1,9 @@
 pub mod context_change;
+pub mod label_symbol;
 pub mod sleigh_symbol;
 pub mod symbol_type;
 
 pub use context_change::ContextChange;
+pub use label_symbol::LabelSymbol;
 pub use sleigh_symbol::SleighSymbol;
 pub use symbol_type::SymbolType;
