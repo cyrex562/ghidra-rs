83e0350849f1835e96e7552b8fd5c642d3a4e40d
Josh Madden <cyrex562@gmail.com>
2026-07-11 02:40:31 -0400

    port: SquashSuperBlock [tests:SKIP] (orig_src/Ghidra/Features/FileFormats/src/main/java/ghidra/file/formats/squashfs/SquashSuperBlock.java)

--- ghidra-rs/src/file/formats/squashfs/mod.rs
+++ ghidra-rs/src/file/formats/squashfs/mod.rs
@@ -1,6 +1,7 @@
 pub mod squash_constants;
 pub mod square_fragment;
 pub mod squash_metablock;
+pub mod squash_super_block;
 
 pub use squash_constants::{
     ALWAYS_FRAGMENT, COMPRESSION_OPTIONS_EXIST, COMPRESSION_TYPE_GZIP, COMPRESSION_TYPE_LZ4,
@@ -18,3 +19,4 @@ pub use squash_constants::{
 };
 pub use square_fragment::SquashFragment;
 pub use squash_metablock::SquashMetablock;
+pub use squash_super_block::SquashSuperBlock;
