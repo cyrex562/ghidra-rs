63559102303faad65d6e23b21a59186a30dcf55c
Josh Madden <cyrex562@gmail.com>
2026-07-11 03:09:35 -0400

    port: RemoveEquateCmd [tests:SKIP] (orig_src/Ghidra/Features/Base/src/main/java/ghidra/app/plugin/core/equate/RemoveEquateCmd.java)

--- ghidra-rs/src/app/cmd/equate/mod.rs
+++ ghidra-rs/src/app/cmd/equate/mod.rs
@@ -1,5 +1,7 @@
 pub mod clear_equate_cmd;
+pub mod remove_equate_cmd;
 pub mod set_equate_cmd;
 
 pub use clear_equate_cmd::ClearEquateCmd;
+pub use remove_equate_cmd::RemoveEquateCmd;
 pub use set_equate_cmd::SetEquateCmd;
