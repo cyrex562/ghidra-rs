983fe536f27c6bb8d509b51ae6a7cc05f8fbd17f
Josh Madden <cyrex562@gmail.com>
2026-07-11 01:18:33 -0400

    port: Hasher32 [tests:SKIP] (orig_src/Ghidra/Features/PDB/src/main/java/ghidra/app/util/bin/format/pdb2/pdbreader/Hasher32.java)

--- ghidra-rs/src/format/pdb2/pdbreader/mod.rs
+++ ghidra-rs/src/format/pdb2/pdbreader/mod.rs
@@ -7,6 +7,7 @@ pub mod c13_inlinee_source_line;
 pub mod delimiter_state;
 pub mod frame_pointer_omission_record;
 pub mod hasher;
+pub mod hasher32;
 pub mod id_ms_parsable;
 pub mod msf;
 pub mod pdb_byte_reader;
