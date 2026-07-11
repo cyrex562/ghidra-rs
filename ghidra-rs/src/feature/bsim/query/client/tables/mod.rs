4aa3dfb9c2442277189f2f858550311660f15e41
Josh Madden <cyrex562@gmail.com>
2026-07-11 00:40:41 -0400

    port: IdfLookupTable [tests:SKIP] (orig_src/Ghidra/Features/BSim/src/main/java/ghidra/features/bsim/query/client/tables/IdfLookupTable.java)

--- ghidra-rs/src/feature/bsim/query/client/tables/mod.rs
+++ ghidra-rs/src/feature/bsim/query/client/tables/mod.rs
@@ -1,10 +1,12 @@
 pub mod cached_statement;
+pub mod idf_lookup_table;
 pub mod optional_table;
 pub mod sql_complex_table;
 pub mod sql_string_table;
 pub mod statement_supplier;
 
 pub use cached_statement::{CachedStatement, SqlStatement};
+pub use idf_lookup_table::{IdfLookupTable, IdfLookupTableConn};
 pub use optional_table::{
     OptionalTable, OptionalTableConn, OptionalTableError, SqlValue, TYPE_INTEGER, TYPE_REAL,
     TYPE_VARCHAR,
