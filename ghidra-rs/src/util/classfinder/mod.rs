159646b96085fec2346fc061821c5d155eff20dd
Josh Madden <cyrex562@gmail.com>
2026-07-11 00:09:21 -0400

    port: ClassTranslator [tests:SKIP] (orig_src/Ghidra/Framework/Generic/src/main/java/ghidra/util/classfinder/ClassTranslator.java)

--- ghidra-rs/src/util/classfinder/mod.rs
+++ ghidra-rs/src/util/classfinder/mod.rs
@@ -2,10 +2,12 @@ pub mod class_file_info;
 pub mod class_filter;
 pub mod class_exclusion_filter;
 pub mod class_location;
+pub mod class_translator;
 pub mod extension_point;
 
 pub use class_file_info::ClassFileInfo;
 pub use class_filter::ClassFilter;
 pub use class_exclusion_filter::ClassExclusionFilter;
 pub use class_location::ClassLocation;
+pub use class_translator::ClassTranslator;
 pub use extension_point::ExtensionPoint;
