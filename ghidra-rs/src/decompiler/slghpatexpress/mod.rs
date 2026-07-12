bf257a7d070da545c401f38aa092e451dfaca563
Josh Madden <cyrex562@gmail.com>
2026-07-11 22:41:34 -0400

    port: StartInstructionValue [tests:SKIP] (orig_src/Ghidra/Framework/SoftwareModeling/src/main/java/ghidra/pcodeCPort/slghpatexpress/StartInstructionValue.java)

--- ghidra-rs/src/decompiler/slghpatexpress/mod.rs
+++ ghidra-rs/src/decompiler/slghpatexpress/mod.rs
@@ -3,6 +3,7 @@ pub mod end_instruction_value;
 pub mod express_utils;
 pub mod next2_instruction_value;
 pub mod pattern_value;
+pub mod start_instruction_value;
 pub mod token_pattern;
 
 pub use constant_value::ConstantValue;
@@ -10,4 +11,5 @@ pub use end_instruction_value::EndInstructionValue;
 pub use express_utils::{advance_combo, build_pattern};
 pub use next2_instruction_value::Next2InstructionValue;
 pub use pattern_value::PatternValue;
+pub use start_instruction_value::StartInstructionValue;
 pub use token_pattern::TokenPattern;
