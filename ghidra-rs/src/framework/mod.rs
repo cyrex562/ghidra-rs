3f9a8efbedd41fb66e10675a14a6bf0b8d7cf369
Josh Madden <cyrex562@gmail.com>
2026-07-11 22:02:08 -0400

    port: ProjectDataService [tests:SKIP] (orig_src/Ghidra/Framework/Project/src/main/java/ghidra/framework/project/ProjectDataService.java)

--- ghidra-rs/src/framework/mod.rs
+++ ghidra-rs/src/framework/mod.rs
@@ -29,6 +29,7 @@ pub use module_initializer::ModuleInitializer;
 pub use os::OperatingSystem;
 pub use pluggable_service_registry_exception::PluggableServiceRegistryException;
 pub use plugintool::{ServiceListener, ServiceProvider};
+pub use project::ProjectDataService;
 pub use service::{PluggableServiceRegistry, PluggableServiceRegistryError};
 pub use shutdown_hook_registry::{ShutdownHook, ShutdownHookRegistry};
 pub use shutdown_priority::ShutdownPriority;
