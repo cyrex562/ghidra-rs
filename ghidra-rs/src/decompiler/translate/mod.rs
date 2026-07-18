f5d4fa3be87bacae86f9ea69f44b77882e635cc4
Josh Madden <cyrex562@gmail.com>
2026-07-17 22:10:21 -0400

    port: BasicSpaceProvider [tests:SKIP] (orig_src/Ghidra/Framework/SoftwareModeling/src/main/java/ghidra/pcodeCPort/translate/BasicSpaceProvider.java)

--- ghidra-rs/src/decompiler/translate/mod.rs
+++ ghidra-rs/src/decompiler/translate/mod.rs
@@ -1,7 +1,9 @@
 pub mod bad_data_error;
+pub mod basic_space_provider;
 pub mod translate;
 pub mod unimpl_error;
 
 pub use bad_data_error::BadDataError;
+pub use basic_space_provider::BasicSpaceProvider;
 pub use translate::Translate;
 pub use unimpl_error::UnimplError;
