b9d922ca1656b34020e895aee83a1995a966aaa1
Josh Madden <cyrex562@gmail.com>
2026-07-17 22:28:45 -0400

    port: OtherSpace [tests:SKIP] (orig_src/Ghidra/Framework/SoftwareModeling/src/main/java/ghidra/pcodeCPort/space/OtherSpace.java)

--- ghidra-rs/src/decompiler/space/mod.rs
+++ ghidra-rs/src/decompiler/space/mod.rs
@@ -1,9 +1,11 @@
 pub mod addr_space;
 pub mod constant_space;
+pub mod other_space;
 pub mod spacetype;
 pub mod unique_space;
 
 pub use addr_space::AddrSpace;
 pub use constant_space::ConstantSpace;
+pub use other_space::OtherSpace;
 pub use spacetype::SpaceType;
 pub use unique_space::UniqueSpace;
