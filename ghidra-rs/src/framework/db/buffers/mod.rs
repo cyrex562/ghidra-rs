529b14b6b1c0550a9b9c143f7f40d907cbc01350
Josh Madden <cyrex562@gmail.com>
2026-07-18 23:26:59 -0400

    descent: BufferFileHandle -> trait (orig_src/Ghidra/Framework/DB/src/main/java/db/buffers/BufferFileHandle.java)

--- ghidra-rs/src/framework/db/buffers/mod.rs
+++ ghidra-rs/src/framework/db/buffers/mod.rs
@@ -1,11 +1,13 @@
 use crate::framework::db::buffer::DataBuffer;
 pub mod block_stream;
 pub mod block_stream_handle;
+pub mod buffer_file_handle;
 pub mod input_block_stream;
 pub mod local_buffer_file;
 pub mod output_block_stream;
 pub use block_stream::BlockStream;
 pub use block_stream_handle::BlockStreamHandle;
+pub use buffer_file_handle::BufferFileHandle;
 pub use input_block_stream::InputBlockStream;
 pub use local_buffer_file::LocalBufferFile;
 pub use output_block_stream::OutputBlockStream;
