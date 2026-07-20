6553af116fc7642888be4a05add047e1511a8821
Josh Madden <cyrex562@gmail.com>
2026-07-20 05:06:41 -0400

    descent: ShortDataType -> trait (orig_src/Ghidra/Framework/SoftwareModeling/src/main/java/ghidra/program/model/data/ShortDataType.java)

--- ghidra-rs/src/program/model/data/mod.rs
+++ ghidra-rs/src/program/model/data/mod.rs
@@ -73,6 +73,7 @@ pub mod render_unicode_settings_definition;
 pub mod resource;
 pub mod rgb16_encoding_settings_definition;
 pub mod rgb32_encoding_settings_definition;
+pub mod short_data_type;
 pub mod source_archive;
 pub mod string_layout_enum;
 pub mod structure;
