834baf1882636373780a0a7957b200adb14c25b7
Josh Madden <cyrex562@gmail.com>
2026-07-20 23:37:18 -0400

    descent: DiamondServiceD -> trait (orig_src/Ghidra/Test/IntegrationTest/src/test.slow/java/ghidra/framework/plugintool/testplugins/DiamondServiceD.java)

--- ghidra-rs/src/framework/plugintool/testplugins/mod.rs
+++ ghidra-rs/src/framework/plugintool/testplugins/mod.rs
@@ -1,5 +1,7 @@
 pub mod circular_plugin_a;
 pub mod circular_service_b;
+pub mod diamond_service_d;
 
 pub use circular_plugin_a::CircularPluginA;
 pub use circular_service_b::CircularServiceB;
+pub use diamond_service_d::DiamondServiceD;
