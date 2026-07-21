50ac600400b1a8cca987109df68e69971fbeb47c
Josh Madden <cyrex562@gmail.com>
2026-07-21 00:37:50 -0400

    descent: BlockStreamServer -> trait (orig_src/Ghidra/Features/GhidraServer/src/main/java/ghidra/server/stream/BlockStreamServer.java)

--- ghidra-rs/src/server/stream/mod.rs
+++ ghidra-rs/src/server/stream/mod.rs
@@ -1,3 +1,5 @@
+pub mod block_stream_server;
 pub mod remote_deflater_output_stream;
 
+pub use block_stream_server::{BlockStreamRegistrationError, BlockStreamServer};
 pub use remote_deflater_output_stream::RemoteDeflaterOutputStream;
