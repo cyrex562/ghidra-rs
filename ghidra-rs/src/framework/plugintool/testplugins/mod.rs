400da8765fe0f9370debdcded4296890ff6870b3
Josh Madden <cyrex562@gmail.com>
2026-07-20 23:44:28 -0400

    descent: InitFailServiceB -> trait (orig_src/Ghidra/Test/IntegrationTest/src/test.slow/java/ghidra/framework/plugintool/testplugins/InitFailServiceB.java)

--- ghidra-rs/src/framework/plugintool/testplugins/mod.rs
+++ ghidra-rs/src/framework/plugintool/testplugins/mod.rs
@@ -3,9 +3,11 @@ pub mod circular_service_b;
 pub mod diamond_service_b;
 pub mod diamond_service_c;
 pub mod diamond_service_d;
+pub mod init_fail_service_b;
 
 pub use circular_plugin_a::CircularPluginA;
 pub use circular_service_b::CircularServiceB;
 pub use diamond_service_b::DiamondServiceB;
 pub use diamond_service_c::DiamondServiceC;
 pub use diamond_service_d::DiamondServiceD;
+pub use init_fail_service_b::InitFailServiceB;
