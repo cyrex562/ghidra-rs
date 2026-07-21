6f2478e7e81e6129f7f55f222cf83c673e3d502d
Josh Madden <cyrex562@gmail.com>
2026-07-20 22:23:49 -0400

    descent: Integer5DataType -> trait (orig_src/Ghidra/Framework/SoftwareModeling/src/main/java/ghidra/program/model/data/Integer5DataType.java)

--- ghidra-rs/src/program/model/data/mod.rs
+++ ghidra-rs/src/program/model/data/mod.rs
@@ -52,6 +52,7 @@ pub mod illegal_rename_exception;
 pub mod int16_t_data_type;
 pub mod integer16_data_type;
 pub mod integer3_data_type;
+pub mod integer5_data_type;
 pub mod int64_t_data_type;
 pub mod int8_t_data_type;
 pub mod invalid_data_type_exception;
