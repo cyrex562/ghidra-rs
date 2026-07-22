22727fe2b81586b256cb0f753b70718e91782dad
Josh Madden <cyrex562@gmail.com>
2026-07-21 23:09:10 -0400

    descent: GhidraProtocolHandler -> trait (orig_src/Ghidra/Framework/Project/src/main/java/ghidra/framework/protocol/ghidra/GhidraProtocolHandler.java)

--- ghidra-rs/src/framework/protocol/ghidra/mod.rs
+++ ghidra-rs/src/framework/protocol/ghidra/mod.rs
@@ -1,5 +1,6 @@
 pub mod default_ghidra_protocol_handler;
 pub mod ghidra_protocol_connector;
+pub mod ghidra_protocol_handler;
 pub mod ghidra_url_connection;
 pub mod repository_info;
 pub mod ghidra_url_result_handler;
@@ -7,6 +8,7 @@ pub mod transient_project_data;
 
 pub use default_ghidra_protocol_handler::DefaultGhidraProtocolHandler;
 pub use ghidra_protocol_connector::GhidraProtocolConnector;
+pub use ghidra_protocol_handler::GhidraProtocolHandler;
 pub use ghidra_url_connection::{
     GhidraURLConnection, GhidraURLContent, SetReadOnlyError, StatusCode,
     GHIDRA_WRAPPED_CONTENT, REPOSITORY_SERVER_CONTENT,
