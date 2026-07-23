76b03d6249d76e33bed0b8a88cd7e428a08d4758
Josh Madden <cyrex562@gmail.com>
2026-07-23 04:41:26 -0400

    descent: SleighInstructionPrototype -> trait (orig_src/Ghidra/Framework/SoftwareModeling/src/main/java/ghidra/app/plugin/processors/sleigh/SleighInstructionPrototype.java)

--- ghidra-rs/src/app/plugin/processors/sleigh/mod.rs
+++ ghidra-rs/src/app/plugin/processors/sleigh/mod.rs
@@ -4,5 +4,6 @@ pub mod pcode_emit_packed;
 pub mod sleigh_exception;
 pub mod sleigh_file_exception;
 pub mod sleigh_file_lock_exception;
+pub mod sleigh_instruction_prototype;
 pub mod sleigh_parser_context;
 pub mod varnode_data;
