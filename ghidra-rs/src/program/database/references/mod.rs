92fb576ef4a0987ca387fe8d8131996c6071f0c2
Josh Madden <cyrex562@gmail.com>
2026-07-23 23:50:39 -0400

    descent: FromAdapter -> trait (orig_src/Ghidra/Framework/SoftwareModeling/src/main/java/ghidra/program/database/references/FromAdapter.java)

--- ghidra-rs/src/program/database/references/mod.rs
+++ ghidra-rs/src/program/database/references/mod.rs
@@ -1,5 +1,6 @@
 pub mod big_ref_list_v0;
 pub mod empty_mem_reference_iterator;
+pub mod from_adapter;
 pub mod record_adapter;
 pub mod ref_list;
 pub mod ref_list_flags_v0;
@@ -8,6 +9,7 @@ pub mod to_adapter;
 
 pub use big_ref_list_v0::BigRefListV0;
 pub use empty_mem_reference_iterator::EmptyMemReferenceIterator;
+pub use from_adapter::FromAdapter;
 pub use record_adapter::RecordAdapter;
 pub use ref_list::RefList;
 pub use ref_list_flags_v0::{decode_source, encode_flags, RefListFlagsV0};
