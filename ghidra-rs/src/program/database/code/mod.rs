46ce3adb1b36bf03cbff07955253238665ec2187
Josh Madden <cyrex562@gmail.com>
2026-07-24 00:56:54 -0400

    descent: CodeUnitDB -> trait (orig_src/Ghidra/Framework/SoftwareModeling/src/main/java/ghidra/program/database/code/CodeUnitDB.java)

--- ghidra-rs/src/program/database/code/mod.rs
+++ ghidra-rs/src/program/database/code/mod.rs
@@ -1,9 +1,11 @@
+pub mod code_unit_db;
 pub mod comments_db_adapter;
 pub mod data_db;
 pub mod data_db_adapter;
 pub mod string_diff;
 pub mod string_diff_utils;
 
+pub use code_unit_db::CodeUnitDb;
 pub use comments_db_adapter::CommentsDBAdapter;
 pub use data_db::{base_data_type, DataDb};
 pub use data_db_adapter::DataDBAdapter;
