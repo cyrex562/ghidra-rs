8ac02d7240ffe2dd2d95e50d0327f31ac2b49c4f
Josh Madden <cyrex562@gmail.com>
2026-07-23 23:32:03 -0400

    descent: RefListFlagsV0 -> trait (orig_src/Ghidra/Framework/SoftwareModeling/src/main/java/ghidra/program/database/references/RefListFlagsV0.java)

--- ghidra-rs/src/program/database/references/mod.rs
+++ ghidra-rs/src/program/database/references/mod.rs
@@ -1,9 +1,11 @@
 pub mod empty_mem_reference_iterator;
 pub mod record_adapter;
+pub mod ref_list_flags_v0;
 pub mod ref_list_v0;
 pub mod to_adapter;
 
 pub use empty_mem_reference_iterator::EmptyMemReferenceIterator;
 pub use record_adapter::RecordAdapter;
+pub use ref_list_flags_v0::{decode_source, encode_flags, RefListFlagsV0};
 pub use ref_list_v0::RefListV0;
 pub use to_adapter::ToAdapter;
