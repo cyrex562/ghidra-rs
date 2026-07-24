b6b0811385b8c6436b8507c2fe12f5e79e4c92ca
Josh Madden <cyrex562@gmail.com>
2026-07-24 01:40:23 -0400

    descent: ParentChildDBAdapter -> trait (orig_src/Ghidra/Framework/SoftwareModeling/src/main/java/ghidra/program/database/module/ParentChildDBAdapter.java)

--- ghidra-rs/src/program/database/module/mod.rs
+++ ghidra-rs/src/program/database/module/mod.rs
@@ -1,6 +1,11 @@
 pub mod fragment_db_adapter;
+pub mod parent_child_db_adapter;
 
 pub use fragment_db_adapter::{
     get_table_name, FragmentDBAdapter, FRAGMENT_COMMENTS_COL, FRAGMENT_NAME_COL,
     FRAGMENT_TABLE_NAME,
+};
+pub use parent_child_db_adapter::{
+    get_table_name as get_parent_child_table_name, ParentChildDBAdapter, CHILD_ID_COL,
+    ORDER_COL, PARENT_CHILD_TABLE_NAME, PARENT_ID_COL,
 };
