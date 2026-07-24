50060e9de3fdce7465ca68fa281bb4c3eb620bd6
Josh Madden <cyrex562@gmail.com>
2026-07-24 00:01:26 -0400

    descent: VariableStorageDBAdapter -> trait (orig_src/Ghidra/Framework/SoftwareModeling/src/main/java/ghidra/program/database/symbol/VariableStorageDBAdapter.java)

--- ghidra-rs/src/program/database/symbol/mod.rs
+++ ghidra-rs/src/program/database/symbol/mod.rs
@@ -3,9 +3,11 @@ pub mod namespace_manager;
 pub mod overlapping_namespace_exception;
 pub mod symbol_db;
 pub mod symbol_manager;
+pub mod variable_storage_db_adapter;
 
 pub use class_symbol::ClassSymbol;
 pub use namespace_manager::NamespaceManagerDB;
 pub use overlapping_namespace_exception::OverlappingNamespaceException;
 pub use symbol_db::SymbolDB;
 pub use symbol_manager::SymbolManagerDB;
+pub use variable_storage_db_adapter::VariableStorageDBAdapter;
