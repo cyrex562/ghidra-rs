84cde87d1112972c4dc1da1622dd0d8c35e90c44
Josh Madden <cyrex562@gmail.com>
2026-07-24 01:45:18 -0400

    descent: ModuleDBAdapter -> trait (orig_src/Ghidra/Framework/SoftwareModeling/src/main/java/ghidra/program/database/module/ModuleDBAdapter.java)

--- ghidra-rs/src/program/database/module/mod.rs
+++ ghidra-rs/src/program/database/module/mod.rs
@@ -1,9 +1,14 @@
 pub mod fragment_db_adapter;
+pub mod module_db_adapter;
 pub mod parent_child_db_adapter;
 
 pub use fragment_db_adapter::{
     get_table_name, FragmentDBAdapter, FRAGMENT_COMMENTS_COL, FRAGMENT_NAME_COL,
     FRAGMENT_TABLE_NAME,
+};
+pub use module_db_adapter::{
+    get_table_name as get_module_table_name, ModuleDBAdapter, MODULE_CHILD_COUNT_COL,
+    MODULE_COMMENTS_COL, MODULE_NAME_COL, MODULE_TABLE_NAME,
 };
 pub use parent_child_db_adapter::{
     get_table_name as get_parent_child_table_name, ParentChildDBAdapter, CHILD_ID_COL,
