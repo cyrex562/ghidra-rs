a7e5bd49c54380a1776379360530952de5e4eb1c
Josh Madden <cyrex562@gmail.com>
2026-07-25 01:01:45 -0400

    descent: ValueSortedMap -> trait (orig_src/Ghidra/Framework/Generic/src/main/java/ghidra/generic/util/datastruct/ValueSortedMap.java)

--- ghidra-rs/src/generic/util/datastruct/mod.rs
+++ ghidra-rs/src/generic/util/datastruct/mod.rs
@@ -1,3 +1,5 @@
 pub mod tree_set_valued_tree_map;
+pub mod value_sorted_map;
 
 pub use tree_set_valued_tree_map::TreeSetValuedTreeMap;
+pub use value_sorted_map::{LesserList, ValueSortedMap, ValueSortedMapEntryList, ValueSortedMapKeyList};
