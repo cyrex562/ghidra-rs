ca4d1c90d23378cd25f3e5cd2f39eab910560b72
Josh Madden <cyrex562@gmail.com>
2026-07-25 04:28:52 -0400

    descent: DataLocationListContext -> trait (orig_src/Ghidra/Features/Base/src/main/java/ghidra/app/context/DataLocationListContext.java)

--- ghidra-rs/src/app/context/mod.rs
+++ ghidra-rs/src/app/context/mod.rs
@@ -1,3 +1,5 @@
+pub mod data_location_list_context;
 pub mod function_supplier_context;
 
+pub use data_location_list_context::DataLocationListContext;
 pub use function_supplier_context::FunctionSupplierContext;
