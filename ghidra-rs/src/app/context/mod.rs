f34d29f98d156cc151a0eb0463e549b2836a3087
Josh Madden <cyrex562@gmail.com>
2026-07-25 04:40:05 -0400

    descent: RestrictedAddressSetContext -> trait (orig_src/Ghidra/Features/Base/src/main/java/ghidra/app/context/RestrictedAddressSetContext.java)

--- ghidra-rs/src/app/context/mod.rs
+++ ghidra-rs/src/app/context/mod.rs
@@ -2,8 +2,10 @@ pub mod data_location_list_context;
 pub mod function_supplier_context;
 pub mod navigatable_action_context;
 pub mod navigation_action_context;
+pub mod restricted_address_set_context;
 
 pub use data_location_list_context::DataLocationListContext;
 pub use function_supplier_context::FunctionSupplierContext;
 pub use navigatable_action_context::NavigatableActionContext;
 pub use navigation_action_context::NavigationActionContext;
+pub use restricted_address_set_context::RestrictedAddressSetContext;
