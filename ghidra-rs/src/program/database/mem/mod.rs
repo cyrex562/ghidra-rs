c4474a560674e755ec60123fc81a3469b84d4580
Josh Madden <cyrex562@gmail.com>
2026-07-26 07:31:12 -0400

    descent: MemoryMapDBAdapter -> trait (orig_src/Ghidra/Framework/SoftwareModeling/src/main/java/ghidra/program/database/mem/MemoryMapDBAdapter.java)

--- ghidra-rs/src/program/database/mem/mod.rs
+++ ghidra-rs/src/program/database/mem/mod.rs
@@ -1,7 +1,9 @@
 pub mod memory_block_db;
 pub mod memory_map_db;
+pub mod memory_map_db_adapter;
 pub mod sub_memory_block;
 
 pub use memory_block_db::MemoryBlockDB;
 pub use memory_map_db::MemoryMapDB;
+pub use memory_map_db_adapter::{MemoryMapDBAdapter, MemoryMapDBAdapterError};
 pub use sub_memory_block::{SubMemoryBlock, SubMemoryBlockError};
