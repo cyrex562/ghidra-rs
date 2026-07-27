88d160720cdc57bf523b928447238ac2ad16521d
Josh Madden <cyrex562@gmail.com>
2026-07-27 01:57:39 -0400

    descent: SourceMapEntryIterator -> trait (orig_src/Ghidra/Framework/SoftwareModeling/src/main/java/ghidra/program/model/sourcemap/SourceMapEntryIterator.java)

--- ghidra-rs/src/program/model/sourcemap/mod.rs
+++ ghidra-rs/src/program/model/sourcemap/mod.rs
@@ -1,3 +1,5 @@
+pub mod source_map_entry_iterator;
 pub mod source_path_transform_record;
 
+pub use source_map_entry_iterator::SourceMapEntryIterator;
 pub use source_path_transform_record::SourcePathTransformRecord;
