b0494126f280efd4576f86125bfccac0955e6185
Josh Madden <cyrex562@gmail.com>
2026-07-27 02:41:10 -0400

    descent: AssemblyResolvedBackfill -> trait (orig_src/Ghidra/Framework/SoftwareModeling/src/main/java/ghidra/app/plugin/assembler/sleigh/sem/AssemblyResolvedBackfill.java)

--- ghidra-rs/src/app/plugin/assembler/sleigh/sem/mod.rs
+++ ghidra-rs/src/app/plugin/assembler/sleigh/sem/mod.rs
@@ -1,5 +1,7 @@
 pub mod assembly_resolution;
+pub mod assembly_resolved_backfill;
 pub mod assembly_resolved_error;
 
 pub use assembly_resolution::AssemblyResolution;
+pub use assembly_resolved_backfill::AssemblyResolvedBackfill;
 pub use assembly_resolved_error::AssemblyResolvedError;
