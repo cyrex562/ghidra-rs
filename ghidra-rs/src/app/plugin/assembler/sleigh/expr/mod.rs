90587fe52f79b6a923c4385a69d452434edb5771
Josh Madden <cyrex562@gmail.com>
2026-07-27 02:34:51 -0400

    descent: SolverHint -> trait (orig_src/Ghidra/Framework/SoftwareModeling/src/main/java/ghidra/app/plugin/assembler/sleigh/expr/SolverHint.java)

--- ghidra-rs/src/app/plugin/assembler/sleigh/expr/mod.rs
+++ ghidra-rs/src/app/plugin/assembler/sleigh/expr/mod.rs
@@ -1,5 +1,7 @@
 pub mod needs_backfill_exception;
 pub mod solver_exception;
+pub mod solver_hint;
 
 pub use needs_backfill_exception::NeedsBackfillException;
 pub use solver_exception::SolverException;
+pub use solver_hint::SolverHint;
