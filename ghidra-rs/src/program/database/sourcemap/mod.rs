09290e522dec042c85eabca2e231d5b3d0a7a496
Josh Madden <cyrex562@gmail.com>
2026-07-27 01:47:56 -0400

    descent: SourceFileAdapter -> trait (orig_src/Ghidra/Framework/SoftwareModeling/src/main/java/ghidra/program/database/sourcemap/SourceFileAdapter.java)

--- ghidra-rs/src/program/database/sourcemap/mod.rs
+++ ghidra-rs/src/program/database/sourcemap/mod.rs
@@ -1,5 +1,7 @@
 pub mod source_file;
+pub mod source_file_adapter;
 pub mod source_file_id_type;
 
 pub use source_file::SourceFile;
+pub use source_file_adapter::{SourceFileAdapter, ID_COL, ID_TYPE_COL, PATH_COL, TABLE_NAME};
 pub use source_file_id_type::SourceFileIdType;
