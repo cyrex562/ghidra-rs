d35ff3e88d3638e22878b7f688bd038b240de28a
Josh Madden <cyrex562@gmail.com>
2026-07-27 03:49:15 -0400

    descent: AssemblyNumericTerminal -> trait (orig_src/Ghidra/Framework/SoftwareModeling/src/main/java/ghidra/app/plugin/assembler/sleigh/symbol/AssemblyNumericTerminal.java)

--- ghidra-rs/src/app/plugin/assembler/sleigh/symbol/mod.rs
+++ ghidra-rs/src/app/plugin/assembler/sleigh/symbol/mod.rs
@@ -1,5 +1,7 @@
 pub mod assembly_extended_non_terminal;
+pub mod assembly_numeric_terminal;
 pub mod assembly_terminal;
 
 pub use assembly_extended_non_terminal::AssemblyExtendedNonTerminal;
+pub use assembly_numeric_terminal::AssemblyNumericTerminal;
 pub use assembly_terminal::AssemblyTerminal;
