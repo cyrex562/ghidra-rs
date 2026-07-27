6e02415739c567cd746a22bad5898de472a56973
Josh Madden <cyrex562@gmail.com>
2026-07-26 23:36:49 -0400

    descent: SleighLanguageDescription -> trait (orig_src/Ghidra/Framework/SoftwareModeling/src/main/java/ghidra/app/plugin/processors/sleigh/SleighLanguageDescription.java)

--- ghidra-rs/src/app/plugin/processors/sleigh/mod.rs
+++ ghidra-rs/src/app/plugin/processors/sleigh/mod.rs
@@ -6,6 +6,7 @@ pub mod sleigh_exception;
 pub mod sleigh_file_exception;
 pub mod sleigh_file_lock_exception;
 pub mod sleigh_instruction_prototype;
+pub mod sleigh_language_description;
 pub mod sleigh_language_file;
 pub mod sleigh_parser_context;
 pub mod varnode_data;
