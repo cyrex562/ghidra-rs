ee7a256d74d24862a03c044501ce481ca92aa7c7
Josh Madden <cyrex562@gmail.com>
2026-07-27 03:39:06 -0400

    descent: AssemblyParseNumericToken -> trait (orig_src/Ghidra/Framework/SoftwareModeling/src/main/java/ghidra/app/plugin/assembler/sleigh/tree/AssemblyParseNumericToken.java)

--- ghidra-rs/src/app/plugin/assembler/sleigh/tree/mod.rs
+++ ghidra-rs/src/app/plugin/assembler/sleigh/tree/mod.rs
@@ -1,3 +1,5 @@
+pub mod assembly_parse_numeric_token;
 pub mod assembly_parse_token;
 
+pub use assembly_parse_numeric_token::AssemblyParseNumericToken;
 pub use assembly_parse_token::AssemblyParseToken;
