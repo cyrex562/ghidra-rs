c8dd90bc073c33c691ef21d73fff3c84b3e9bdf7
Josh Madden <cyrex562@gmail.com>
2026-07-27 23:22:54 -0400

    descent: AbstractAssemblyState -> trait (orig_src/Ghidra/Framework/SoftwareModeling/src/main/java/ghidra/app/plugin/assembler/sleigh/sem/AbstractAssemblyState.java)

--- ghidra-rs/src/app/plugin/assembler/sleigh/sem/mod.rs
+++ ghidra-rs/src/app/plugin/assembler/sleigh/sem/mod.rs
@@ -1,9 +1,11 @@
+pub mod abstract_assembly_state;
 pub mod abstract_assembly_tree_resolver;
 pub mod assembly_resolution;
 pub mod assembly_resolved_backfill;
 pub mod assembly_resolved_error;
 pub mod default_assembly_resolved_backfill;
 
+pub use abstract_assembly_state::AbstractAssemblyState;
 pub use abstract_assembly_tree_resolver::AbstractAssemblyTreeResolver;
 pub use assembly_resolution::AssemblyResolution;
 pub use assembly_resolved_backfill::AssemblyResolvedBackfill;
