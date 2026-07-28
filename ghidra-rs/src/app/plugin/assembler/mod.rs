61e6862872fab841c64238ccc3a527b5664a8cb4
Josh Madden <cyrex562@gmail.com>
2026-07-27 22:15:07 -0400

    descent: GenericAssemblerBuilder -> trait (orig_src/Ghidra/Framework/SoftwareModeling/src/main/java/ghidra/app/plugin/assembler/GenericAssemblerBuilder.java)

--- ghidra-rs/src/app/plugin/assembler/mod.rs
+++ ghidra-rs/src/app/plugin/assembler/mod.rs
@@ -4,6 +4,7 @@ pub mod assembly_selection_error;
 pub mod assembly_selector;
 pub mod assembly_semantic_exception;
 pub mod generic_assembler;
+pub mod generic_assembler_builder;
 pub mod sleigh;
 
 pub use assembly_error::AssemblyError;
@@ -12,3 +13,4 @@ pub use assembly_selection_error::AssemblySelectionError;
 pub use assembly_selector::{AssemblySelector, Selection};
 pub use assembly_semantic_exception::AssemblySemanticException;
 pub use generic_assembler::{AssembleError, AssembleLineError, GenericAssembler};
+pub use generic_assembler_builder::GenericAssemblerBuilder;
