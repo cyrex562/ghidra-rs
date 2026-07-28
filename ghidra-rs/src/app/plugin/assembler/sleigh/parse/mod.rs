fdb8c333a292d555ce0ccb1d8917e361428a784e
Josh Madden <cyrex562@gmail.com>
2026-07-27 22:30:07 -0400

    descent: AssemblyParseTransitionTable -> trait (orig_src/Ghidra/Framework/SoftwareModeling/src/main/java/ghidra/app/plugin/assembler/sleigh/parse/AssemblyParseTransitionTable.java)

--- ghidra-rs/src/app/plugin/assembler/sleigh/parse/mod.rs
+++ ghidra-rs/src/app/plugin/assembler/sleigh/parse/mod.rs
@@ -1,3 +1,5 @@
 pub mod assembly_parse_result;
+pub mod assembly_parse_transition_table;
 
 pub use assembly_parse_result::AssemblyParseResult;
+pub use assembly_parse_transition_table::{AssemblyParseTransitionTable, TableEntry};
