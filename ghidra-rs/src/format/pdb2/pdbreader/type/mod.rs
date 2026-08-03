af7034ff29963f1f93760402098de3f685608776
Josh Madden <cyrex562@gmail.com>
2026-08-03 06:09:22 -0400

    descent: CallingConvention -> trait (orig_src/Ghidra/Features/PDB/src/main/java/ghidra/app/util/bin/format/pdb2/pdbreader/type/CallingConvention.java)

--- ghidra-rs/src/format/pdb2/pdbreader/type/mod.rs
+++ ghidra-rs/src/format/pdb2/pdbreader/type/mod.rs
@@ -1,9 +1,11 @@
 pub mod abstract_cobol0_ms_type;
 pub mod abstract_dimensioned_array_const_bounds_lower_upper_ms_type;
+pub mod calling_convention;
 pub mod ms_type;
 pub mod ms_type_field;
 
 pub use abstract_cobol0_ms_type::AbstractCobol0MsType;
 pub use abstract_dimensioned_array_const_bounds_lower_upper_ms_type::AbstractDimensionedArrayConstBoundsLowerUpperMsType;
+pub use calling_convention::{CallingConvention, StandardCallingConvention};
 pub use ms_type::MsType;
 pub use ms_type_field::MsTypeField;
