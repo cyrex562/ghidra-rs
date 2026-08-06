8105b2aa928dca331649ba1fbe2df19e4eb4e7c5
Josh Madden <cyrex562@gmail.com>
2026-08-06 00:37:35 -0400

    descent: TraceStackReference -> trait (orig_src/Ghidra/Debug/Framework-TraceModeling/src/main/java/ghidra/trace/model/symbol/TraceStackReference.java)

--- ghidra-rs/src/trace/model/symbol/mod.rs
+++ ghidra-rs/src/trace/model/symbol/mod.rs
@@ -1,5 +1,7 @@
 pub mod trace_offset_reference;
 pub mod trace_shifted_reference;
+pub mod trace_stack_reference;
 
 pub use trace_offset_reference::TraceOffsetReference;
 pub use trace_shifted_reference::TraceShiftedReference;
+pub use trace_stack_reference::TraceStackReference;
