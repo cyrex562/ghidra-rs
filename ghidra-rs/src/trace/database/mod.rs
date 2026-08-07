f51a877585a250bdb8676c88143727d746aa0ae2
Josh Madden <cyrex562@gmail.com>
2026-08-06 22:15:15 -0400

    descent: DBTraceManager -> trait (orig_src/Ghidra/Debug/Framework-TraceModeling/src/main/java/ghidra/trace/database/DBTraceManager.java)

--- ghidra-rs/src/trace/database/mod.rs
+++ ghidra-rs/src/trace/database/mod.rs
@@ -1,6 +1,8 @@
 pub mod db_trace_direct_change_listener;
+pub mod db_trace_manager;
 pub mod program;
 pub mod symbol;
 pub mod target;
 
 pub use db_trace_direct_change_listener::DbTraceDirectChangeListener;
+pub use db_trace_manager::DBTraceManager;
