7bb05f1d5c8ae9e4fee42f3d246ba714dc992aaa
Josh Madden <cyrex562@gmail.com>
2026-08-06 21:15:12 -0400

    descent: TraceDefinedDataView -> trait (orig_src/Ghidra/Debug/Framework-TraceModeling/src/main/java/ghidra/trace/model/listing/TraceDefinedDataView.java)

--- ghidra-rs/src/trace/model/listing/mod.rs
+++ ghidra-rs/src/trace/model/listing/mod.rs
@@ -4,6 +4,7 @@ pub mod trace_code_unit;
 pub mod trace_code_units_view;
 pub mod trace_data;
 pub mod trace_data_view;
+pub mod trace_defined_data_view;
 
 pub use trace_base_code_units_view::TraceBaseCodeUnitsView;
 pub use trace_base_defined_units_view::TraceBaseDefinedUnitsView;
@@ -11,3 +12,4 @@ pub use trace_code_unit::TraceCodeUnit;
 pub use trace_code_units_view::TraceCodeUnitsView;
 pub use trace_data::TraceData;
 pub use trace_data_view::TraceDataView;
+pub use trace_defined_data_view::TraceDefinedDataView;
