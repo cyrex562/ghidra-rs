d86f69feada14a19a072de4722a64033875ad83f
Josh Madden <cyrex562@gmail.com>
2026-08-08 00:29:39 -0400

    descent: DBTraceEquate -> trait (orig_src/Ghidra/Debug/Framework-TraceModeling/src/main/java/ghidra/trace/database/symbol/DBTraceEquate.java)

--- ghidra-rs/src/trace/database/symbol/mod.rs
+++ ghidra-rs/src/trace/database/symbol/mod.rs
@@ -1,5 +1,7 @@
+pub mod db_trace_equate;
 pub mod db_trace_equate_manager;
 pub mod db_trace_snap_selected_reference_space;
 
+pub use db_trace_equate::DBTraceEquate;
 pub use db_trace_equate_manager::DBTraceEquateManager;
 pub use db_trace_snap_selected_reference_space::DBTraceSnapSelectedReferenceSpace;
