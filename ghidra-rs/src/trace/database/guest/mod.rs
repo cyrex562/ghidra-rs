914bd2a8e7238567b7799e4d9e40b9b82bf01708
Josh Madden <cyrex562@gmail.com>
2026-08-08 05:12:32 -0400

    descent: DBTracePlatformManager -> trait (orig_src/Ghidra/Debug/Framework-TraceModeling/src/main/java/ghidra/trace/database/guest/DBTracePlatformManager.java)

--- ghidra-rs/src/trace/database/guest/mod.rs
+++ ghidra-rs/src/trace/database/guest/mod.rs
@@ -1,3 +1,5 @@
+pub mod db_trace_platform_manager;
 pub mod internal_trace_platform;
 
+pub use db_trace_platform_manager::DBTracePlatformManager;
 pub use internal_trace_platform::InternalTracePlatform;
