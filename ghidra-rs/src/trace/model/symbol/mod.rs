b3f3d42575fd1b60d29958a94721969f4d2f47c4
Josh Madden <cyrex562@gmail.com>
2026-08-07 21:23:57 -0400

    descent: TraceSymbolWithLifespan -> trait (orig_src/Ghidra/Debug/Framework-TraceModeling/src/main/java/ghidra/trace/model/symbol/TraceSymbolWithLifespan.java)

--- ghidra-rs/src/trace/model/symbol/mod.rs
+++ ghidra-rs/src/trace/model/symbol/mod.rs
@@ -5,6 +5,7 @@ pub mod trace_reference;
 pub mod trace_shifted_reference;
 pub mod trace_stack_reference;
 pub mod trace_symbol;
+pub mod trace_symbol_with_lifespan;
 
 pub use trace_class_symbol::TraceClassSymbol;
 pub use trace_namespace_symbol::TraceNamespaceSymbol;
@@ -13,3 +14,4 @@ pub use trace_reference::TraceReference;
 pub use trace_shifted_reference::TraceShiftedReference;
 pub use trace_stack_reference::TraceStackReference;
 pub use trace_symbol::TraceSymbol;
+pub use trace_symbol_with_lifespan::TraceSymbolWithLifespan;
