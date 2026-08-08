a4611b18d87c253728b8ba27213df2d8e71f7c48
Josh Madden <cyrex562@gmail.com>
2026-08-08 00:14:44 -0400

    descent: TracePropertyMap -> trait (orig_src/Ghidra/Debug/Framework-TraceModeling/src/main/java/ghidra/trace/model/property/TracePropertyMap.java)

--- ghidra-rs/src/trace/model/property/mod.rs
+++ ghidra-rs/src/trace/model/property/mod.rs
@@ -1,3 +1,5 @@
+pub mod trace_property_map;
 pub mod trace_property_map_operations;
 
+pub use trace_property_map::TracePropertyMap;
 pub use trace_property_map_operations::TracePropertyMapOperations;
