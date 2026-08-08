c67959e92e448fc9cf204838c6928f7e0ca49a81
Josh Madden <cyrex562@gmail.com>
2026-08-07 22:26:17 -0400

    descent: TraceEquateReference -> trait (orig_src/Ghidra/Debug/Framework-TraceModeling/src/main/java/ghidra/trace/model/symbol/TraceEquateReference.java)

--- ghidra-rs/src/trace/model/symbol/mod.rs
+++ ghidra-rs/src/trace/model/symbol/mod.rs
@@ -1,5 +1,6 @@
 pub mod trace_class_symbol;
 pub mod trace_class_symbol_view;
+pub mod trace_equate_reference;
 pub mod trace_label_symbol;
 pub mod trace_label_symbol_view;
 pub mod trace_namespace_symbol;
@@ -17,6 +18,7 @@ pub mod trace_symbol_with_location_view;
 
 pub use trace_class_symbol::TraceClassSymbol;
 pub use trace_class_symbol_view::TraceClassSymbolView;
+pub use trace_equate_reference::TraceEquateReference;
 pub use trace_label_symbol::TraceLabelSymbol;
 pub use trace_label_symbol_view::TraceLabelSymbolView;
 pub use trace_namespace_symbol::TraceNamespaceSymbol;
