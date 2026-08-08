19ecb5bc4c986a5eec1aa13cd532b6e58741e842
Josh Madden <cyrex562@gmail.com>
2026-08-08 04:16:58 -0400

    descent: DBTraceCodeSpace -> trait (orig_src/Ghidra/Debug/Framework-TraceModeling/src/main/java/ghidra/trace/database/listing/DBTraceCodeSpace.java)

--- ghidra-rs/src/trace/database/listing/mod.rs
+++ ghidra-rs/src/trace/database/listing/mod.rs
@@ -1,6 +1,7 @@
 pub mod abstract_base_db_trace_code_units_memory_view;
 pub mod abstract_db_trace_code_unit;
 pub mod abstract_single_db_trace_code_units_view;
+pub mod db_trace_code_space;
 pub mod db_trace_data;
 pub mod db_trace_data_adapter;
 pub mod db_trace_defined_units_view;
@@ -9,6 +10,7 @@ pub mod internal_trace_base_defined_units_view;
 pub use abstract_base_db_trace_code_units_memory_view::AbstractBaseDBTraceCodeUnitsMemoryView;
 pub use abstract_db_trace_code_unit::AbstractDBTraceCodeUnit;
 pub use abstract_single_db_trace_code_units_view::AbstractSingleDBTraceCodeUnitsView;
+pub use db_trace_code_space::DBTraceCodeSpace;
 pub use db_trace_data::DBTraceData;
 pub use db_trace_data_adapter::DBTraceDataAdapter;
 pub use db_trace_defined_units_view::DBTraceDefinedUnitsView;
