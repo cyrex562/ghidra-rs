de1bd8110b4b64a18c5bf87f30ef6604afd1e984
Josh Madden <cyrex562@gmail.com>
2026-08-08 02:47:34 -0400

    descent: AbstractDBTraceSymbol -> trait (orig_src/Ghidra/Debug/Framework-TraceModeling/src/main/java/ghidra/trace/database/symbol/AbstractDBTraceSymbol.java)

--- ghidra-rs/src/trace/database/symbol/mod.rs
+++ ghidra-rs/src/trace/database/symbol/mod.rs
@@ -1,8 +1,10 @@
+pub mod abstract_db_trace_symbol;
 pub mod db_trace_equate;
 pub mod db_trace_equate_manager;
 pub mod db_trace_namespace_symbol;
 pub mod db_trace_snap_selected_reference_space;
 
+pub use abstract_db_trace_symbol::AbstractDBTraceSymbol;
 pub use db_trace_equate::DBTraceEquate;
 pub use db_trace_equate_manager::DBTraceEquateManager;
 pub use db_trace_namespace_symbol::DBTraceNamespaceSymbol;
