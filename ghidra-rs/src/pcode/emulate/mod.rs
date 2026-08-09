55c7ff57a6a612f13a8af19443bf00c243efcb85
Josh Madden <cyrex562@gmail.com>
2026-08-09 01:02:55 -0400

    descent: BreakCallBack -> trait (orig_src/Ghidra/Framework/Emulation/src/main/java/ghidra/pcode/emulate/BreakCallBack.java)

--- ghidra-rs/src/pcode/emulate/mod.rs
+++ ghidra-rs/src/pcode/emulate/mod.rs
@@ -1,8 +1,10 @@
+pub mod break_callback;
 pub mod break_table;
 pub mod emulate_execution_state;
 pub mod instruction_decode_exception;
 pub mod unimplemented_instruction_exception;
 
+pub use break_callback::BreakCallBack;
 pub use break_table::BreakTable;
 pub use instruction_decode_exception::InstructionDecodeException;
 pub use unimplemented_instruction_exception::UnimplementedInstructionException;
