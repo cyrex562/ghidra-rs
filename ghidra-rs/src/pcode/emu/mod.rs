36ce3067e056f277d09861260f0608483101298f
Josh Madden <cyrex562@gmail.com>
2026-08-08 23:35:58 -0400

    descent: PcodeStateInitializer -> trait (orig_src/Ghidra/Framework/Emulation/src/main/java/ghidra/pcode/emu/PcodeStateInitializer.java)

--- ghidra-rs/src/pcode/emu/mod.rs
+++ ghidra-rs/src/pcode/emu/mod.rs
@@ -1,8 +1,10 @@
 pub mod instruction_decoder;
 pub mod jit;
+pub mod pcode_state_initializer;
 pub mod symz3;
 pub mod sys;
 pub mod taint;
 pub mod unix;
 
 pub use instruction_decoder::InstructionDecoder;
+pub use pcode_state_initializer::PcodeStateInitializer;
