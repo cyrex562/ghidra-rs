785625990e21a0f680ebac48a334501f50a3d41f
Josh Madden <cyrex562@gmail.com>
2026-08-08 22:28:30 -0400

    descent: PcodeFormatter -> trait (orig_src/Ghidra/Framework/SoftwareModeling/src/main/java/ghidra/app/util/pcode/PcodeFormatter.java)

--- ghidra-rs/src/app/util/pcode/mod.rs
+++ ghidra-rs/src/app/util/pcode/mod.rs
@@ -1,5 +1,10 @@
 pub mod abstract_appender;
 pub mod appender;
+pub mod pcode_formatter;
 
 pub use abstract_appender::{AbstractAppender, AbstractAppenderBase};
 pub use appender::Appender;
+pub use pcode_formatter::{
+    get_pcode_op_template_log, get_pcode_op_templates, get_pcode_op_templates_log, get_varnode_tpl,
+    PcodeFormatter,
+};
