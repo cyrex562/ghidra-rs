57fce3f776e5fd953d87c69b65b858fbb9258ebb
Josh Madden <cyrex562@gmail.com>
2026-08-09 21:49:00 -0400

    descent: PcodeDebuggerMemoryAccess -> trait (orig_src/Ghidra/Debug/Debugger-api/src/main/java/ghidra/debug/api/emulation/PcodeDebuggerMemoryAccess.java)

--- ghidra-rs/src/debug/api/emulation/mod.rs
+++ ghidra-rs/src/debug/api/emulation/mod.rs
@@ -1,3 +1,5 @@
 pub mod pcode_debugger_data_access;
+pub mod pcode_debugger_memory_access;
 
 pub use pcode_debugger_data_access::PcodeDebuggerDataAccess;
+pub use pcode_debugger_memory_access::PcodeDebuggerMemoryAccess;
