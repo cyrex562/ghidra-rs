12862e22e65d53efa323baa8dd06fc5326575283
Josh Madden <cyrex562@gmail.com>
2026-08-09 21:57:25 -0400

    descent: PcodeDebuggerAccess -> trait (orig_src/Ghidra/Debug/Debugger-api/src/main/java/ghidra/debug/api/emulation/PcodeDebuggerAccess.java)

--- ghidra-rs/src/debug/api/emulation/mod.rs
+++ ghidra-rs/src/debug/api/emulation/mod.rs
@@ -1,7 +1,9 @@
+pub mod pcode_debugger_access;
 pub mod pcode_debugger_data_access;
 pub mod pcode_debugger_memory_access;
 pub mod pcode_debugger_registers_access;
 
+pub use pcode_debugger_access::PcodeDebuggerAccess;
 pub use pcode_debugger_data_access::PcodeDebuggerDataAccess;
 pub use pcode_debugger_memory_access::PcodeDebuggerMemoryAccess;
 pub use pcode_debugger_registers_access::PcodeDebuggerRegistersAccess;
