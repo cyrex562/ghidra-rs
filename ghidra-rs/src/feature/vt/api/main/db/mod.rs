328c168e66531693448d9d4979ab41a8c38cb38c
Josh Madden <cyrex562@gmail.com>
2026-08-10 23:41:43 -0400

    descent: VTAddressCorrelatorAdapter -> trait (orig_src/Ghidra/Features/VersionTracking/src/main/java/ghidra/feature/vt/api/db/VTAddressCorrelatorAdapter.java)

--- ghidra-rs/src/feature/vt/api/main/db/mod.rs
+++ ghidra-rs/src/feature/vt/api/main/db/mod.rs
@@ -1,9 +1,14 @@
 pub mod deleted_match;
+pub mod vt_address_correlator_adapter;
 pub mod vt_match_set_table_db_adapter;
 pub mod vt_match_table_db_adapter;
 pub mod vt_match_tag_db_adapter;
 
 pub use deleted_match::DeletedMatch;
+pub use vt_address_correlator_adapter::{
+    ColumnDescription as AddressCorrelationTableColumnDescription, VTAddressCorrelatorAdapter,
+    VTAddressCorrelatorAdapterBase, TABLE_NAME as ADDRESS_CORRELATION_TABLE_TABLE_NAME,
+};
 pub use vt_match_set_table_db_adapter::{
     ColumnDescription as MatchSetTableColumnDescription, VTMatchSetTableDBAdapter,
     VTMatchSetTableDBAdapterBase, TABLE_NAME as MATCH_SET_TABLE_TABLE_NAME,
