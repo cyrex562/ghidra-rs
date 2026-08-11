b7122d62035d0e788347d429e62768dc8b6a5590
Josh Madden <cyrex562@gmail.com>
2026-08-10 22:55:30 -0400

    descent: MarkupItemStorage -> trait (orig_src/Ghidra/Features/VersionTracking/src/main/java/ghidra/feature/vt/api/impl/MarkupItemStorage.java)

--- ghidra-rs/src/feature/vt/api/implementation/mod.rs
+++ ghidra-rs/src/feature/vt/api/implementation/mod.rs
@@ -1,9 +1,11 @@
+pub mod markup_item_storage;
 pub mod program_correlator_info_fake;
 pub mod vt_change_manager;
 pub mod vt_event;
 pub mod vt_program_correlator_info;
 pub mod version_tracking_change_record;
 
+pub use markup_item_storage::MarkupItemStorage;
 pub use program_correlator_info_fake::ProgramCorrelatorInfoFake;
 pub use vt_change_manager::deprecated as vt_change_manager_deprecated;
 pub use vt_event::VtEvent;
