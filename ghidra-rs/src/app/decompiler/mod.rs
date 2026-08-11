d3a66b4d8142fe093471d450b5fa317298e334df
Josh Madden <cyrex562@gmail.com>
2026-08-10 21:05:58 -0400

    descent: ClangNode -> trait (orig_src/Ghidra/Features/Decompiler/src/main/java/ghidra/app/decompiler/ClangNode.java)

--- ghidra-rs/src/app/decompiler/mod.rs
+++ ghidra-rs/src/app/decompiler/mod.rs
@@ -1,8 +1,10 @@
+pub mod clang_node;
 pub mod clang_token_group;
 pub mod component;
 pub mod decompile_exception;
 pub mod decompiled_function;
 
+pub use clang_node::ClangNode;
 pub use clang_token_group::ClangTokenGroup;
 pub use decompile_exception::DecompileException;
 pub use decompiled_function::DecompiledFunction;
