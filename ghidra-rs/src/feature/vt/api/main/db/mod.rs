10946f6828c929cd5ec08f9036c1b42b5a560121
Josh Madden <cyrex562@gmail.com>
2026-08-10 23:16:40 -0400

    descent: VTMatchTagDBAdapter -> trait (orig_src/Ghidra/Features/VersionTracking/src/main/java/ghidra/feature/vt/api/db/VTMatchTagDBAdapter.java)

--- ghidra-rs/src/feature/vt/api/main/db/mod.rs
+++ ghidra-rs/src/feature/vt/api/main/db/mod.rs
@@ -1,3 +1,7 @@
 pub mod deleted_match;
+pub mod vt_match_tag_db_adapter;
 
 pub use deleted_match::DeletedMatch;
+pub use vt_match_tag_db_adapter::{
+    ColumnDescription, VTMatchTagDBAdapter, VTMatchTagDBAdapterBase, TABLE_NAME,
+};
