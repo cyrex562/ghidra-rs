4923ac1d94a24fd4408f15ed41b1fb4fcb7b30a7
Josh Madden <cyrex562@gmail.com>
2026-08-12 02:20:56 -0400

    descent: SimpleVarHandler -> trait (orig_src/Ghidra/Framework/Emulation/src/main/java/ghidra/pcode/emu/jit/alloc/SimpleVarHandler.java)

--- ghidra-rs/src/pcode/emu/jit/alloc/mod.rs
+++ ghidra-rs/src/pcode/emu/jit/alloc/mod.rs
@@ -1,7 +1,9 @@
 pub mod jvm_local;
+pub mod simple_var_handler;
 pub mod sub_var_handler;
 pub mod var_handler;
 
 pub use jvm_local::JvmLocal;
+pub use simple_var_handler::SimpleVarHandler;
 pub use sub_var_handler::SubVarHandler;
 pub use var_handler::{name_vn, VarHandler};
