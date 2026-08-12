28a2d7a04dbd8e2296451790ea7f5a0bd2561ed5
Josh Madden <cyrex562@gmail.com>
2026-08-11 21:50:19 -0400

    descent: MpAccessGen -> trait (orig_src/Ghidra/Framework/Emulation/src/main/java/ghidra/pcode/emu/jit/gen/access/MpAccessGen.java)

---
+++ ghidra-rs/src/pcode/emu/jit/gen/access/mod.rs
@@ -0,0 +1,3 @@
+pub mod mp_access_gen;
+
+pub use mp_access_gen::MpAccessGen;
--- ghidra-rs/src/pcode/emu/jit/gen/mod.rs
+++ ghidra-rs/src/pcode/emu/jit/gen/mod.rs
@@ -1,8 +1,10 @@
+pub mod access;
 pub mod field_req;
 pub mod instance_field_req;
 pub mod opnd;
 pub mod util;
 
+pub use access::MpAccessGen;
 pub use field_req::FieldReq;
 pub use instance_field_req::InstanceFieldReq;
 pub use opnd::LocalOpnd;
