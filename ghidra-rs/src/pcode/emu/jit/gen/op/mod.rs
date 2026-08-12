148cfa6c8d01d53a270b8290e404fcdf81a93f7a
Josh Madden <cyrex562@gmail.com>
2026-08-12 03:03:21 -0400

    descent: IntOpUnOpGen -> trait (orig_src/Ghidra/Framework/Emulation/src/main/java/ghidra/pcode/emu/jit/gen/op/IntOpUnOpGen.java)

--- ghidra-rs/src/pcode/emu/jit/gen/op/mod.rs
+++ ghidra-rs/src/pcode/emu/jit/gen/op/mod.rs
@@ -2,10 +2,12 @@ pub mod bin_op_gen;
 pub mod c_branch_op_gen;
 pub mod int_bitwise_bin_op_gen;
 pub mod int_op_bin_op_gen;
+pub mod int_op_un_op_gen;
 pub mod un_op_gen;
 
 pub use bin_op_gen::{BinOpGen, TakeOut};
 pub use c_branch_op_gen::CBranchOpGen;
 pub use int_bitwise_bin_op_gen::IntBitwiseBinOpGen;
 pub use int_op_bin_op_gen::IntOpBinOpGen;
+pub use int_op_un_op_gen::IntOpUnOpGen;
 pub use un_op_gen::UnOpGen;
