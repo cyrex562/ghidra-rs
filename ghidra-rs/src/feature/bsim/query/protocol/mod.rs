8d037117c7c95ce1201e23f797e6de16e23ff7dd
Josh Madden <cyrex562@gmail.com>
2026-08-16 04:03:32 -0400

    descent: PrewarmRequest -> trait (orig_src/Ghidra/Features/BSim/src/main/java/ghidra/features/bsim/query/protocol/PrewarmRequest.java)

--- ghidra-rs/src/feature/bsim/query/protocol/mod.rs
+++ ghidra-rs/src/feature/bsim/query/protocol/mod.rs
@@ -1,11 +1,13 @@
 pub mod adjust_vector_index;
 pub mod insert_request;
 pub mod password_change;
+pub mod prewarm_request;
 pub mod query_response_record;
 pub mod response_optional_exist;
 
 pub use adjust_vector_index::AdjustVectorIndex;
 pub use insert_request::InsertRequest;
 pub use password_change::PasswordChange;
+pub use prewarm_request::PrewarmRequest;
 pub use query_response_record::{QueryResponseRecord, QueryResponseRecordBase};
 pub use response_optional_exist::ResponseOptionalExist;
