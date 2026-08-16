f598beb9ee054129616cb213441cbbcd9daa65ea
Josh Madden <cyrex562@gmail.com>
2026-08-16 04:42:17 -0400

    descent: QueryVectorId -> trait (orig_src/Ghidra/Features/BSim/src/main/java/ghidra/features/bsim/query/protocol/QueryVectorId.java)

--- ghidra-rs/src/feature/bsim/query/protocol/mod.rs
+++ ghidra-rs/src/feature/bsim/query/protocol/mod.rs
@@ -9,6 +9,7 @@ pub mod query_nearest;
 pub mod query_nearest_vector;
 pub mod query_response_record;
 pub mod query_update;
+pub mod query_vector_id;
 pub mod response_optional_exist;
 
 pub use adjust_vector_index::AdjustVectorIndex;
@@ -22,4 +23,5 @@ pub use query_nearest::QueryNearest;
 pub use query_nearest_vector::QueryNearestVector;
 pub use query_response_record::{QueryResponseRecord, QueryResponseRecordBase};
 pub use query_update::QueryUpdate;
+pub use query_vector_id::QueryVectorId;
 pub use response_optional_exist::ResponseOptionalExist;
