1610572c1568da6ff6c5747eca9e9faeb823849a
Josh Madden <cyrex562@gmail.com>
2026-08-16 05:17:31 -0400

    descent: ScoreCaching -> trait (orig_src/Ghidra/Features/BSim/src/main/java/ghidra/features/bsim/query/client/ScoreCaching.java)

--- ghidra-rs/src/feature/bsim/query/client/mod.rs
+++ ghidra-rs/src/feature/bsim/query/client/mod.rs
@@ -2,9 +2,11 @@ pub mod b_sim_sql_clause;
 pub mod cancelled_sql_exception;
 pub mod no_database_exception;
 pub mod row_key_sql;
+pub mod score_caching;
 pub mod tables;
 
 pub use b_sim_sql_clause::BSimSqlClause;
 pub use cancelled_sql_exception::CancelledSqlException;
 pub use no_database_exception::NoDatabaseException;
 pub use row_key_sql::RowKeySQL;
+pub use score_caching::ScoreCaching;
