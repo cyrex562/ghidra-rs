a9d1a8417d91b4de9a5e31cbd8fffa791fa7a47d
Josh Madden <cyrex562@gmail.com>
2026-08-16 23:14:10 -0400

    descent: AnnotatedEmuSyscallUseropLibrary -> trait (orig_src/Ghidra/Features/SystemEmulation/src/main/java/ghidra/pcode/emu/sys/AnnotatedEmuSyscallUseropLibrary.java)

--- ghidra-rs/src/pcode/emu/sys/mod.rs
+++ ghidra-rs/src/pcode/emu/sys/mod.rs
@@ -1,6 +1,11 @@
+pub mod annotated_emu_syscall_userop_library;
 pub mod emu_file_contents;
 pub mod emu_syscall_library;
 
+pub use annotated_emu_syscall_userop_library::{
+    bind_syscalls, AnnotatedEmuSyscallUseropLibrary, AnnotatedEmuSyscallUseropLibraryBase,
+    EmuSyscallBinding,
+};
 pub use emu_file_contents::EmuFileContents;
 pub use emu_syscall_library::{
     load_syscall_convention_map, load_syscall_function_map, load_syscall_number_map,
