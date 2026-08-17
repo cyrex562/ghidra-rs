1c580fa409c7587bcde8396ec98a419807dd051d
Josh Madden <cyrex562@gmail.com>
2026-08-16 23:32:29 -0400

    descent: EmuUnixFileSystem -> trait (orig_src/Ghidra/Features/SystemEmulation/src/main/java/ghidra/pcode/emu/unix/EmuUnixFileSystem.java)

--- ghidra-rs/src/pcode/emu/unix/mod.rs
+++ ghidra-rs/src/pcode/emu/unix/mod.rs
@@ -1,9 +1,11 @@
 pub mod emu_unix_file;
 pub mod emu_unix_file_descriptor;
 pub mod emu_unix_file_stat;
+pub mod emu_unix_file_system;
 pub mod emu_unix_user;
 
 pub use emu_unix_file::EmuUnixFile;
 pub use emu_unix_file_descriptor::{EmuUnixFileDescriptor, FD_STDERR, FD_STDIN, FD_STDOUT};
 pub use emu_unix_file_stat::{EmuUnixFileStat, MODE_R, MODE_W, MODE_X};
+pub use emu_unix_file_system::{EmuUnixFileSystem, OpenFlag};
 pub use emu_unix_user::EmuUnixUser;
