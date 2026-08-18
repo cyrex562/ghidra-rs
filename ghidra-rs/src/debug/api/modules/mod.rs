12f0971b8e502a98ee1edf12fbc52af4d8c82c28
Josh Madden <cyrex562@gmail.com>
2026-08-18 03:16:21 -0400

    descent: ModuleMapProposal -> trait (orig_src/Ghidra/Debug/Debugger-api/src/main/java/ghidra/debug/api/modules/ModuleMapProposal.java)

--- ghidra-rs/src/debug/api/modules/mod.rs
+++ ghidra-rs/src/debug/api/modules/mod.rs
@@ -2,8 +2,10 @@ pub mod debugger_address_translator;
 pub mod debugger_static_mapping_change_listener;
 pub mod map_entry;
 pub mod map_proposal;
+pub mod module_map_proposal;
 
 pub use debugger_address_translator::DebuggerAddressTranslator;
 pub use debugger_static_mapping_change_listener::DebuggerStaticMappingChangeListener;
 pub use map_entry::MapEntry;
 pub use map_proposal::MapProposal;
+pub use module_map_proposal::{ModuleMapEntry, ModuleMapProposal};
