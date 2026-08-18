ea22d215982470ffea1fd8d1791f57fc01634b5a
Josh Madden <cyrex562@gmail.com>
2026-08-18 06:03:10 -0400

    descent: CodeSarifMgr -> trait (orig_src/Ghidra/Features/Sarif/src/main/java/sarif/managers/CodeSarifMgr.java)

--- ghidra-rs/src/sarif/managers/mod.rs
+++ ghidra-rs/src/sarif/managers/mod.rs
@@ -1,5 +1,7 @@
 pub mod bookmarks_sarif_mgr;
+pub mod code_sarif_mgr;
 pub mod memory_map_bytes_file;
 
 pub use bookmarks_sarif_mgr::BookmarksSarifMgr;
+pub use code_sarif_mgr::CodeSarifMgr;
 pub use memory_map_bytes_file::MemoryMapBytesFile;
