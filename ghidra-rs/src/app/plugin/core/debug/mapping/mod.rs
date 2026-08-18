769c3cf9fffd55be518760cd9980e228a128843b
Josh Madden <cyrex562@gmail.com>
2026-08-18 05:04:12 -0400

    descent: DebuggerPlatformOffer -> trait (orig_src/Ghidra/Debug/Debugger/src/main/java/ghidra/app/plugin/core/debug/mapping/DebuggerPlatformOffer.java)

---
+++ ghidra-rs/src/app/plugin/core/debug/mapping/mod.rs
@@ -0,0 +1,3 @@
+pub mod debugger_platform_offer;
+
+pub use debugger_platform_offer::DebuggerPlatformOffer;
--- ghidra-rs/src/app/plugin/core/debug/mod.rs
+++ ghidra-rs/src/app/plugin/core/debug/mod.rs
@@ -1,6 +1,7 @@
 pub mod client;
 pub mod event;
 pub mod gui;
+pub mod mapping;
 pub mod opinion;
 pub mod service;
 pub mod stack;
