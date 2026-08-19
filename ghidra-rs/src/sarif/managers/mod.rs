5d8d81fb7cd790b791c1620e9937f9bad364bf0f
Josh Madden <cyrex562@gmail.com>
2026-08-18 21:59:18 -0400

    descent: CommentsSarifMgr -> trait (orig_src/Ghidra/Features/Sarif/src/main/java/sarif/managers/CommentsSarifMgr.java)

--- ghidra-rs/src/sarif/managers/mod.rs
+++ ghidra-rs/src/sarif/managers/mod.rs
@@ -1,7 +1,9 @@
 pub mod bookmarks_sarif_mgr;
 pub mod code_sarif_mgr;
+pub mod comments_sarif_mgr;
 pub mod memory_map_bytes_file;
 
 pub use bookmarks_sarif_mgr::BookmarksSarifMgr;
 pub use code_sarif_mgr::CodeSarifMgr;
+pub use comments_sarif_mgr::CommentsSarifMgr;
 pub use memory_map_bytes_file::MemoryMapBytesFile;
