5be172c47b14cfad12d55fa60970eaae8fa58f82
Josh Madden <cyrex562@gmail.com>
2026-08-19 03:32:22 -0400

    descent: GccExceptionAnalyzer -> trait (orig_src/Ghidra/Features/Base/src/main/java/ghidra/app/plugin/exceptionhandlers/gcc/GccExceptionAnalyzer.java)

--- ghidra-rs/src/app/plugin/exceptionhandlers/gcc/mod.rs
+++ ghidra-rs/src/app/plugin/exceptionhandlers/gcc/mod.rs
@@ -2,6 +2,7 @@ pub mod dwarf_decode_context;
 pub mod dwarf_eh_data_application_mode;
 pub mod dwarf_eh_data_decode_format;
 pub mod dwarf_eh_decoder;
+pub mod gcc_exception_analyzer;
 pub mod sections;
 pub mod structures;
 
@@ -9,5 +10,6 @@ pub use dwarf_decode_context::DwarfDecodeContext;
 pub use dwarf_eh_data_application_mode::DwarfEhDataApplicationMode;
 pub use dwarf_eh_data_decode_format::DwarfEhDataDecodeFormat;
 pub use dwarf_eh_decoder::DwarfEHDecoder;
+pub use gcc_exception_analyzer::GccExceptionAnalyzer;
 pub use sections::{CieSource, CieSourceError};
 pub use structures::{ExceptionHandlerFrameException, LSDAActionRecord};
